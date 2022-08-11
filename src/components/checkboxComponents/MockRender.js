6d90b9432aaafcd4c9fab395d5023c2e7a74bbab
amaury <tissot.amaury@gmail.com>
2022-08-11 11:52:25 +0200

    Working on sending data to state

--- src/components/checkboxComponents/MockRender.js
+++ src/components/checkboxComponents/MockRender.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
+// Mock data for development
 const mockData = [
   {
     title: "MockData",
@@ -60,7 +61,7 @@ const MockRender = ({ dataState, setDataState }) => {
   // Mock data state for development
   const mockDataState = [
     {
-      MockRootId1: true,
+      MockRootId1: false,
       MockRootId2: true,
       MockRootId3: true,
     },
@@ -77,14 +78,17 @@ const MockRender = ({ dataState, setDataState }) => {
     trueArray.push(mockDataState[0][accessItem]);
   };
 
+  // Hook state for "oui" checkbox : generate an array base on quiz length
   const [ouiCheckedState, setOuiCheckedState] = useState(
-    new Array(plusValues[0].tree.length).fill(false)
+    new Array(mockData[0].quiz.length).fill(false)
   );
+
+  // Hook state for "non" checkbox : generate an array base on quiz length
   const [nonCheckedState, setNonCheckedState] = useState(
-    new Array(plusValues[0].tree.length).fill(false)
+    new Array(mockData[0].quiz.length).fill(false)
   );
 
-
+  // Handle "oui" checkbox : update the state based on the index of the current question
   const handleOuiCheck = (position) => {
     const updatedCheckedState = ouiCheckedState.map((item, index) =>
       index === position ? !item : item
@@ -93,6 +97,7 @@ const MockRender = ({ dataState, setDataState }) => {
     setOuiCheckedState(updatedCheckedState);
   };
 
+  // Handle "oui" checkbox : update the state based on the index of the current question
   const handleNonCheck = (position) => {
     const updatedCheckedState = nonCheckedState.map((item, index) =>
       index === position ? !item : item
@@ -101,8 +106,79 @@ const MockRender = ({ dataState, setDataState }) => {
     setNonCheckedState(updatedCheckedState);
   };
 
-  const sendData = (event) => {
+  const sendDataOnClick = (event) => {
     setDataState([...dataState, { [event.target.id]: event.target.value }]);
+  };
+
+  const keyExists = (obj, key) => {
+    if (!obj || (typeof obj !== "object" && !Array.isArray(obj))) {
+      return false;
+    } else if (obj.hasOwnProperty(key)) {
+      return true;
+    } else if (Array.isArray(obj)) {
+      for (let i = 0; i < obj.length; i++) {
+        const result = keyExists(obj[i], key);
+        if (result) {
+          return result;
+        }
+      }
+    } else {
+      for (const k in obj) {
+        const result = keyExists(obj[k], key);
+        if (result) {
+          return result;
+        }
+      }
+    }
+
+    return false;
+  };
+
+  // Function who detect if the id exist in the dataState
+  const verifyIfIdExist = (state) => {
+    console.log(keyExists(state, "MockRootId1"));
+  };
+
+  //Buffer array
+  const bufferArray = [];
+
+  //Function who send all the object id into a buffer array
+  const sendIdToBuffer = () => {
+    mockData.
+  };
+
+  // Checkbox who display function and send value to dataState
+  const checkboxJSX = (index, quizItem) => {
+    return (
+      <div>
+        <label>
+          <input
+            onChange={(e) => {
+              handleOuiCheck(index);
+              sendDataOnClick(e);
+            }}
+            type="checkbox"
+            checked={ouiCheckedState[index]}
+            id={quizItem.id}
+            value={true}
+          />
+          oui
+        </label>
+        <label>
+          <input
+            onChange={(e) => {
+              handleNonCheck(index);
+              sendDataOnClick(e);
+            }}
+            type="checkbox"
+            checked={nonCheckedState[index]}
+            id={quizItem.id}
+            value={false}
+          />
+          non
+        </label>
+      </div>
+    );
   };
 
   // Display the tree element when the state is true
@@ -151,43 +227,19 @@ const MockRender = ({ dataState, setDataState }) => {
         <div key={index}>
           {item.quiz.map((quizItem, index) => (
             <div key={index}>
-              {quizItem.access.map((accessItem, index) => (
-                <div key={index}>
+              {quizItem.access.map((accessItem, accessIndex) => (
+                <div key={accessIndex}>
                   {pushArray(accessItem)}
-                  {console.log("trueArray", index, trueArray)}
-                  {console.log("Access array length", quizItem.access.length)}
+                  {/* {console.log("trueArray", index, trueArray)}
+                  {console.log("Access array length", quizItem.access.length)} */}
 
                   {quizItem.access[0] === "keystone" && (
                     <div>
-                      {<h3>quizItem.question</h3>}
-                      <div>
-                        <label>
-                          <input
-                            onChange={(e) => {
-                              handleOuiCheck(index);
-                              sendData(e);
-                            }}
-                            type="checkbox"
-                            checked={ouiCheckedState[index]}
-                            id={tree.id}
-                            value={true}
-                          />
-                          oui
-                        </label>
-                        <label>
-                          <input
-                            onChange={(e) => {
-                              handleNonCheck(index);
-                              sendData(e);
-                            }}
-                            type="checkbox"
-                            checked={nonCheckedState[index]}
-                            id={tree.id}
-                            value={false}
-                          />
-                          non
-                        </label>
-                      </div>
+                      <h3>Keystone : {quizItem.question}</h3>
+                      <p>id : {quizItem.id}</p>
+                      {/* {console.log("ouiCheckedState", ouiCheckedState)}
+                      {console.log("nonCheckedState", nonCheckedState)} */}
+                      {checkboxJSX(index, quizItem)}
                     </div>
                   )}
 
@@ -195,24 +247,34 @@ const MockRender = ({ dataState, setDataState }) => {
                     (allAreTrue(trueArray) ? (
                       quizItem.logic === "root" && (
                         <div>
-                          {quizItem.question}
+                          <h3>Root : {quizItem.question}</h3>
+                          <p>id : {quizItem.id}</p>
+                          {checkboxJSX(index, quizItem)}
+
                           {trueArray.splice(0, trueArray.length)}
-                          {console.log("TRUE trueArray has been clean")}
+                          {/* {console.log("TRUE trueArray has been clean")} */}
                         </div>
                       )
                     ) : (
                       <div>
                         {trueArray.splice(0, trueArray.length)}
-                        {console.log("FALSE trueArray has been clean")}
+                        {quizItem.access[0] !== "keystone" &&
+                          quizItem.logic === "root" &&
+                          console.log("")}
+                        {/* TODO = send value to state */}
+                        {/* {console.log("FALSE trueArray has been clean")} */}
                       </div>
                     ))}
                 </div>
               ))}
             </div>
           ))}
-          {console.log(trueArray)}
         </div>
       ))}
+      {console.log(dataState)}
+      <button onClick={() => verifyIfIdExist(dataState)}>
+        CHECK IF THE KEY EXIST
+      </button>
     </div>
   );
 };
