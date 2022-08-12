602264be916e1beea66c2aef3236eeb79ba866c9
amaury <tissot.amaury@gmail.com>
2022-08-12 18:49:30 +0200

    Comment & delete unnecessary code

--- src/components/checkboxComponents/CheckBoxRendering.js
+++ src/components/checkboxComponents/CheckBoxRendering.js
@@ -1,43 +1,22 @@
 import React, { useState } from "react";
-import RenderRoot from "./RenderRoot";
-import RenderTree from "./RenderTree";
-import QuizTitle from "./QuizTitle";
+// import RenderRoot from "./RenderRoot";
+// import RenderTree from "./RenderTree";
+// import QuizTitle from "./QuizTitle";
 import MockRender from "./MockRender";
 import plusValues from "../../data";
 
 const CheckBoxRendering = () => {
   const [dataState, setDataState] = useState({});
-  const [rootSubmit, setRootSubmit] = useState(false);
-  const [treeSubmit, setTreeSubmit] = useState(false);
+  // const [rootSubmit, setRootSubmit] = useState(false);
+  // const [treeSubmit, setTreeSubmit] = useState(false);
 
   return (
     <div>
-      {/* <QuizTitle />
-
-      {rootSubmit ? (
-        treeSubmit ? (
-          <div>treeSubmit true</div>
-        ) : (
-          <RenderTree
-            dataState={dataState}
-            setDataState={setDataState}
-            treeSubmit={treeSubmit}
-            setTreeSubmit={setTreeSubmit}
-          />
-        )
-      ) : (
-        <RenderRoot
-          setDataState={setDataState}
-          dataState={dataState}
-          rootSubmit={rootSubmit}
-          setRootSubmit={setRootSubmit}
-        />
-      )} */}
-
-
-      <MockRender setDataState={setDataState}
-          dataState={dataState} QCMObject={plusValues}/>
-      
+      <MockRender
+        setDataState={setDataState}
+        dataState={dataState}
+        QCMObject={plusValues}
+      />
     </div>
   );
 };
