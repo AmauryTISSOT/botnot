fca0f1524ba76e3dc6aa4f9d6559beffd251b99a
amaury <tissot.amaury@gmail.com>
2022-08-12 18:15:53 +0200

    Refresh data object

--- src/data.js
+++ src/data.js
@@ -1,13 +1,52 @@
 const plusValues = [
   {
-    title: "Plus-values immobilières",
-    root: {
+  title: "Plus-values immobilières",
+  quiz: [
+    {
       id: "plusValuesResidencePrincipale",
       type: "binary",
       logic: "root",
-      pointer: "tree",
+      access: ["keystone"],
       question: "Le bien vendu constitue-t-il votre résidence principale ?",
     },
+    {
+      id: "plusValuesRemploiPrix",
+      type: "binary",
+      logic: "tree",
+      access: ["plusValuesResidencePrincipale"],
+      question:
+        "Le vendez-vous pour remployer le prix dans l'achat de votre résidence principale ?",
+    },
+
+    {
+      id: "plusValuesInvalide",
+      type: "binary",
+      logic: "tree",
+      access: ["plusValuesResidencePrincipale"],
+      question: "Etes-vous retraité ou invalide ?",
+    },
+
+    {
+      id: "plusValuesAmelioration",
+      type: "binary",
+      logic: "tree",
+      access: ["plusValuesResidencePrincipale"],
+      question:
+        "Avez-vous effectué des travaux de surélévation, construction ou d'amélioration ?",
+    },
+  ]
+
+}
+
+
+
+
+
+
+
+
+
+  {
     tree: [
       {
         id: "plusValuesRemploiPrix",
