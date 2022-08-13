2389acabcc135359e18c853ff87ce9ff0af43b8d
amaury <tissot.amaury@gmail.com>
2022-08-13 19:33:00 +0200

    Remove useless import

--- src/components/checkboxComponents/CheckBoxRendering.js
+++ src/components/checkboxComponents/CheckBoxRendering.js
@@ -3,8 +3,7 @@ import React, { useState } from "react";
 // import RenderTree from "./RenderTree";
 // import QuizTitle from "./QuizTitle";
 import MockRender from "./MockRender";
-import {plusValues} from "../../data";
-import { MockData } from "../../data";
+import { maison } from "../../data";
 
 const CheckBoxRendering = () => {
   const [dataState, setDataState] = useState({});
@@ -16,7 +15,7 @@ const CheckBoxRendering = () => {
       <MockRender
         setDataState={setDataState}
         dataState={dataState}
-        QCMObject={plusValues}
+        QCMObject={maison}
       />
     </div>
   );
