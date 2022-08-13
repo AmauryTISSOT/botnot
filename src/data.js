61f601f95c9afc7827fa284bb463e88ef116e8ad
amaury <tissot.amaury@gmail.com>
2022-08-13 11:47:26 +0200

    Add new object

--- src/data.js
+++ src/data.js
@@ -90,3 +90,126 @@ export const MockData = [
   },
 ];
 
+export const etatSousSolPollution = [
+  {
+    title: "Etat du sous-sol - pollution",
+    quiz: [
+      {
+        id: "activitesPolluantes",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question:
+          "Avez-vous exploité des activités polluantes sur le terrain ?",
+      },
+      {
+        id: "activitesPolluantesPasse",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question:
+          "A votre connaissance, des activités polluantes ont-elles été effectuées sur le terrain dans le passé ou des déchets ont-ils pu être entreposés ?",
+      },
+      {
+        id: "activitesPolluantesVoisin",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question:
+          "Avez-vous connaissance d'activités polluantes dans le voisinage ?",
+      },
+      {
+        id: "carrieres",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question:
+          "Avez-vous connaissance de carrières souterraines dans le voisinage ?",
+      },
+      {
+        id: "zoneInondable",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question: "Le bien est-il situé en zone inondable ?",
+      },
+    ],
+  },
+];
+
+export const servitude = [
+  {
+    title: "Servitudes",
+    quiz: [
+      {
+        id: "servitude",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question:
+          "Avez-vous connaissance de servitudes particulières sur votre propriété ? (servitude de passage, d'alignement, etc.)",
+      },
+      {
+        id: "murSeparatif",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question: "Avez-vous un mur séparatif avec vos voisins ?",
+      },
+      {
+        id: "terrainEnPente",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question: "Votre terrain est-il en pente ?",
+      },
+    ],
+  },
+];
+
+export const assainissement = [
+  {
+    title: "Assainissement",
+    quiz: [
+      {
+        id: "assainissementIndividuel",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question:
+          "L'assainissement de votre bien s'effectue-t-il par un système individuel ( ex: fosse septique ) ?",
+      },
+      //TODO: prévoir un switch si la keystone est = false
+      {
+        id: "assainissementToutAEgout",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question: "Votre bien est-il raccordé au tout à l'égout ?",
+      },
+      {
+        id: "assainissementMiseEnDemeure",
+        type: "binary",
+        logic: "root",
+        access: ["keystone"],
+        question:
+          "Avez-vous reçu une mise en demeure de faire des travaux sur votre installation ?",
+      },
+      {
+        id: "assainissementVidange",
+        type: "binary",
+        logic: "tree",
+        access: ["assainissementIndividuel"],
+        question:
+          "La dernière vidange de votre fosse septique a-t-elle était effectuée recemment ?",
+      },
+      {
+        id: "assainissementInstallation",
+        type: "binary",
+        logic: "tree",
+        access: ["assainissementToutAEgout"],
+        question: "Votre installation est-elle en bon état ?",
+      },
+    ],
+  },
+];
