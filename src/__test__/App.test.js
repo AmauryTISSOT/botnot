4547afb20c361fbf74825ceb7501e6bcdfc84d53
amaury <tissot.amaury@gmail.com>
2022-08-15 20:08:09 +0200

    Delete App.test.js

--- src/__test__/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-describe("App component", () => {
-  test("App render correctly", () => {
-    const {container} = render(<App/>);
-    expect(container).toMatchSnapshot();
-  })
-})
