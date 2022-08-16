edb33927553f7dcb4ad71be0612b840a38b4b31a
amaury <tissot.amaury@gmail.com>
2022-08-16 12:43:44 +0200

    Create testing file for QCMchoice component

---
+++ src/__test__/QCMchoice.test.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom"; // optional
+import QCMChoice from "../components/checkboxComponents/QCMChoice";
+
+    const MockQCMChoice = () => {
+        const [choiceState, setChoiceState] = useState("")
+        return (
+            <>
+            <QCMChoice setChoiceState={setChoiceState}/>
+            <div>test : {choiceState}</div>
+            </>
+        );
+      };
+
+
+describe("QCMchoice component unit testing", () => {
+
+  test("The component should render the title", () => {
+    render(<MockQCMChoice />);
+    const titleElement = screen.getByText(/type de biens vendus/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test("The component should render 3 buttons", () => {
+    render(<MockQCMChoice />);
+    const buttonsElements = screen.getAllByRole("button");
+    expect(buttonsElements.length).toBe(3);
+  });
+
+  test("Clicking on maison button should change choiceState", () => {
+    render(<MockQCMChoice />);
+    const buttonMaison = screen.getByRole("button", {name: /maison/i});
+    fireEvent.click(buttonMaison)
+    const mockDiv = screen.getByText(/test : maison/i)
+    expect(mockDiv).toBeInTheDocument();
+  });
+
+  test("Clicking on appartement button should change choiceState", () => {
+    render(<MockQCMChoice />);
+    const buttonAppartement = screen.getByRole("button", {name: /appartement/i});
+    fireEvent.click(buttonAppartement)
+    const mockDiv = screen.getByText(/test : appartement/i)
+    expect(mockDiv).toBeInTheDocument();
+  });
+
+  test("Clicking on terrain button should change choiceState", () => {
+    render(<MockQCMChoice />);
+    const buttonTerrain = screen.getByRole("button", {name: /terrain/i});
+    fireEvent.click(buttonTerrain)
+    const mockDiv = screen.getByText(/test : terrain/i)
+    expect(mockDiv).toBeInTheDocument();
+  });
+
+});
