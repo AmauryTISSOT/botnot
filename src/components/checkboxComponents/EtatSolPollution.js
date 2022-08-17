cd1fa9ac64ee9623fb6c27b2c146a6e2323eff1e
amaury <tissot.amaury@gmail.com>
2022-08-17 18:52:04 +0200

    Create EtatSolPollution component

---
+++ src/components/checkboxComponents/EtatSolPollution.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const EtatSolPollution = () => {
+  return (
+    <>
+      <h1>ETAT DU SOUS-SOL - POLLUTION</h1>
+      <div>Avez-vous exploité des activités polluantes sur le terrain ?</div>
+      <div>
+        A votre connaissance, des activités polluantes ont-elles été effectuées
+        sur le terrain par le passé ?
+      </div>
+      <div>
+        Avez-vous connaissance d'activités polluantes dans le voisinage ?
+      </div>
+      <div>
+        Avez-vous connaissance de carrières souterraines à l'aplomb de votre
+        terrain ?
+      </div>
+      <div>Votre maison est-elle située en zone inondable ?</div>
+    </>
+  );
+};
+
+export default EtatSolPollution;
