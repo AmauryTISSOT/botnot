6e6d14e353e0659326606c78b8a6f80959ebe9de
amaury <tissot.amaury@gmail.com>
2022-08-17 19:20:02 +0200

    Create Assainissement component

---
+++ src/components/checkboxComponents/Assainissement.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Assainissement = () => {
+  return (
+    <>
+    <h1>ASSAINISSEMENT</h1>
+    <div>L'assainissement de votre bien s'effectue-t-il:</div>
+        <div>- par un système individuel ?</div>
+        <p>Indiquez la date de la dernière vidange</p>
+        <div>- par un raccordement au "tout à l'égout" ?</div>
+    <div>Avez-vous reçu une mise en demeure de faire des travaux sur votre installation ?</div>
+    </>
+  )
+}
+
+export default Assainissement
