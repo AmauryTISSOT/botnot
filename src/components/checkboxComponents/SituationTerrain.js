744876ea54cf816b0e553928d339c10b2e409543
amaury <tissot.amaury@gmail.com>
2022-08-17 18:48:40 +0200

    Create Situation Terrain component

---
+++ src/components/checkboxComponents/SituationTerrain.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SituationTerrain = () => {
+  return (
+    <>
+      <h1>SITUATION DU TERRAIN</h1>
+      <div>Votre maison dépend-elle d'un lotissement ?</div>
+      <div>Si oui, existe-t-il une association syndicale qui fonctionne ?</div>
+      <p>
+        Si tel est le cas, précisé ici les noms et coordonnées du président de
+        l'association
+      </p>
+      <div>Votre maison dépend-elle d'une copropriété horizontale ?</div>
+      <p>Si oui, il convient de nous indiquer les noms et adresses du syndic</p>
+      <div>
+        Votre terrain a-t-il fait l'objet d'un procès verbal de bornage ?
+      </div>
+    </>
+  );
+};
+
+export default SituationTerrain;
