2ad21d41f41190c9c64d1a6823ec86c1fde85f4f
amaury <tissot.amaury@gmail.com>
2022-08-17 18:54:04 +0200

    Create Servitude component

---
+++ src/components/checkboxComponents/Servitude.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Servitude = () => {
+  return (
+    <>
+      <h1>SERVITUDES</h1>
+      <div>
+        Avez-vous connaissance de servitudes particulières sur votre propriété ?
+      </div>
+      <p>Si oui, savez-vous qui a la charge de son entretien ?</p>
+    </>
+  );
+};
+
+export default Servitude;
