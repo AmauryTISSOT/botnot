fc6643c9ab9f4f4793d8f10c83f18d492a190b4f
amaury <tissot.amaury@gmail.com>
2022-08-18 20:05:04 +0200

    Create CustomerQuiz component

--- src/components/CustomerQuiz.js
+++ src/components/CustomerQuiz.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import Assainissement from "./CustomerQuiz/Assainissement";
+import AvantageFiscal from "./CustomerQuiz/AvantageFiscal";
+import Chauffage from "./CustomerQuiz/Chauffage";
+import ContratSurBien from "./CustomerQuiz/ContratSurBien";
+import EtatSolPollution from "./CustomerQuiz/EtatSolPollution";
+import PlusValues from "./CustomerQuiz/PlusValues";
+import Pret from "./CustomerQuiz/Pret";
+import Servitude from "./CustomerQuiz/Servitude";
+import SituationBatiment from "./CustomerQuiz/SituationBatiment";
+import SituationTerrain from "./CustomerQuiz/SituationTerrain";
 
 const CustomerQuiz = () => {
-  return (
-    <>
-    </>
-  )
-}
 
-export default CustomerQuiz
+  const [customerQuizState, setCustomerQuizState] = useState("")
+
+  return (
+    <div className="App">
+      <SituationTerrain />
+      <EtatSolPollution />
+      <Servitude />
+      <Assainissement />
+      <SituationBatiment />
+      <ContratSurBien />
+      <AvantageFiscal />
+      <Chauffage />
+      <Pret />
+      <PlusValues />
+    </div>
+  );
+};
+
+export default CustomerQuiz;
