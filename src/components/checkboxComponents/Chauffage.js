95d14d374e80966ad3f6e217a20efd1957e49f82
amaury <tissot.amaury@gmail.com>
2022-08-18 14:15:50 +0200

    Create Chauffage component

---
+++ src/components/checkboxComponents/Chauffage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Chauffage = () => {
+  return (
+    <>
+      <h1>CHAUFFAGE</h1>
+      <div>Le bien : </div>
+      <div>- possède t-il une cuve à mazout ?</div>
+      <div>- est-il raccordé au gaz de ville ?</div>
+      <div>- est-il raccordé à une citerne à gaz ?</div>
+      <div>- êtes-vous propriétaire de la citerne à gaz ?</div>
+      <div>- êtes-vous locataire de la citerne ?</div>
+      <p>Le cas échéant, indiquez les coordonnées de la société :</p>
+    </>
+  );
+};
+
+export default Chauffage;
