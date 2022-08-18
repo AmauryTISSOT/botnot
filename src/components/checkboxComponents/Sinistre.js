d86f51f66ab7c57a5012a2255232e3a7ab0a6acf
amaury <tissot.amaury@gmail.com>
2022-08-18 14:31:47 +0200

    Create Sinistre component

---
+++ src/components/checkboxComponents/Sinistre.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Sinistre = () => {
+  return (
+    <>
+      <h1>SINISTRE INDEMNISE</h1>
+      <div>
+        Y'a t-il eu une indemnisation d'assurance d'un sinistre survenue suite à
+        un evenement reconnu comme catastrophe naturelle ?
+      </div>
+    </>
+  );
+};
+
+export default Sinistre;
