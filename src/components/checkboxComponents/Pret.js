427981adb03f228920c07b691e1bd71802100ead
amaury <tissot.amaury@gmail.com>
2022-08-18 14:20:41 +0200

    Create Pret component

---
+++ src/components/checkboxComponents/Pret.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Pret = () => {
+  return (
+    <>
+      <h1>PRETS-HYPOTHEQUES</h1>
+      <div>
+        Avez-vous soucrit un ou plusieurs prêts avec hypothèques sur le bien à
+        vendre ?
+      </div>
+      <div>
+        Avez-vous un crédit-relais dans l'attente de la vente de ce bien ?
+      </div>
+      <div>
+        Une procédure de saisie immobilière est-elle en cours sur le bien à
+        vendre ?
+      </div>
+    </>
+  );
+};
+
+export default Pret;
