1246d05cc624b607298a749a2ee0bb785fc86ac3
amaury <tissot.amaury@gmail.com>
2022-08-18 14:27:45 +0200

    Create PlusValues component

---
+++ src/components/checkboxComponents/PlusValues.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PlusValues = () => {
+  return (
+    <>
+      <h1>PLUS-VALUES</h1>
+      <div>Le bien vendu constitue-t-il votre résidence principale ?</div>
+      <div>
+        Le vendez-vous pour remployer le prix dans l'achat de votre résidence
+        principale ?
+      </div>
+      <div>Etes-vous retraité ou invalide de condition modeste ?</div>
+      <div>
+        Si le bien n'est pas actuellement votre résidence principale, avez-vous
+        effectué des travaux de surélévation, de construction ou d'amélioration
+        ?
+      </div>
+    </>
+  );
+};
+
+export default PlusValues;
