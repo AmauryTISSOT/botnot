df5b99af8b42ad9b0dabef685290d8b607cab851
amaury <tissot.amaury@gmail.com>
2022-08-18 14:08:17 +0200

    Create AvantageFiscal component

---
+++ src/components/checkboxComponents/AvantageFiscal.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AvantageFiscal = () => {
+  return (
+    <>
+      <h1>AVANTAGE FISCAL</h1>
+      <div>
+        LE bien a-t-il fait l'objet d'un engagement de location lié à un
+        aventage fiscal ? (lois Besson, Malraux, Robien, Duflot, Pinel...
+        subventions ANAH)
+      </div>
+      <p>Si oui, lequel :</p>
+    </>
+  );
+};
+
+export default AvantageFiscal;
