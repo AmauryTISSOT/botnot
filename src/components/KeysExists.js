3b16b361823b0f918d79869fb3b5c8afaa143485
amaury <tissot.amaury@gmail.com>
2022-08-29 17:42:45 +0200

    Create keyExists function

---
+++ src/components/KeysExists.js
@@ -0,0 +1,27 @@
+
+// Function who return true if the key exist in object
+const keyExists = (obj, key) => {
+  if (!obj || (typeof obj !== "object" && !Array.isArray(obj))) {
+    return false;
+  } else if (obj.hasOwnProperty(key)) {
+    return true;
+  } else if (Array.isArray(obj)) {
+    for (let i = 0; i < obj.length; i++) {
+      const result = keyExists(obj[i], key);
+      if (result) {
+        return result;
+      }
+    }
+  } else {
+    for (const k in obj) {
+      const result = keyExists(obj[k], key);
+      if (result) {
+        return result;
+      }
+    }
+  }
+
+  return false;
+};
+
+export default keyExists;
