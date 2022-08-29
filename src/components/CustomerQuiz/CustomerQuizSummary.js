a0eb2c32f5432fe8cd8a4c10e6365557723ebf3c
amaury <tissot.amaury@gmail.com>
2022-08-29 17:43:13 +0200

    Fix textArea rendering

--- src/components/CustomerQuiz/CustomerQuizSummary.js
+++ src/components/CustomerQuiz/CustomerQuizSummary.js
@@ -1,7 +1,9 @@
 import React from "react";
+import keyExists from "../KeysExists";
 
+
+// This component render a summary of the CustomerQuiz state.
 const CustomerQuizSummary = ({ dataState }) => {
-
   const situationTerrainAnswer = {
     situationTerrainLotissement: "Le bien dépend d'un lotissement",
     situationTerrainAssociationSyndicale: "Il existe une association syndicale",
@@ -14,93 +16,121 @@ const CustomerQuizSummary = ({ dataState }) => {
   };
 
   const EtatSolPollutionAnswer = {
-    EtatSolActivitePolluante : "Le vendeur a effectué des activités polluantes sur le bien",
-    EtatSolActivitePolluantePasse: "Des activités polluantes ont été effectués sur le bien par le passé",
-    EtatSolActivitePolluanteVoisinage: "Il existe des activités polluantes dans le voisinage",
+    EtatSolActivitePolluante:
+      "Le vendeur a effectué des activités polluantes sur le bien",
+    EtatSolActivitePolluantePasse:
+      "Des activités polluantes ont été effectués sur le bien par le passé",
+    EtatSolActivitePolluanteVoisinage:
+      "Il existe des activités polluantes dans le voisinage",
     EtatSolCarriere: "Il existe une carrière souterraine sous le bien",
     EtatSolZoneInondable: "Le bien est situé en zone inondable",
   };
 
   const servitudeAnswer = {
-    servitude : "Une servitude grève le bien",
-    servitudeNom: "textArea"
+    servitude: "Une servitude grève le bien",
+    servitudeNom: "textArea",
   };
 
   const assainissementAnswer = {
-    assainissementSytemeIndividuel: "L'assainissement du bien s'effectue par un système individuel (fosse septique)",
+    assainissementSytemeIndividuel:
+      "L'assainissement du bien s'effectue par un système individuel (fosse septique)",
     assainissementDateVidange: "textArea",
     assainissementRaccordementEgout: "Le bien est raccordé au tout à l'égout",
-    assainissementMiseDemeure: "Le vendeur a reçu une mise en demeure d'effectuer des travaux sur son installation",
+    assainissementMiseDemeure:
+      "Le vendeur a reçu une mise en demeure d'effectuer des travaux sur son installation",
   };
 
   const situationBatimentAnswer = {
-    situationBatimentConstructionInitiale: "Le vendeur a fait construire le bâtiment vendu",
-    situationBatimentConstructionInitiale10ans: "La construction initiale a moins de 10 ans",
-    situationBatimentConstructionInitialeDommageOuvrage: "Le vendeur a souscrit une assurance dommage-ouvrage",
-    situationBatimentTravauxPost: "Des travaux postérieurs à la construction initiale ont été effectués par le vendeur",
-    situationBatimentTravauxPostAutorisation: "Ces travaux postérieurs à la construction ont fait l'objet d'autorisations d'urbanisme"
+    situationBatimentConstructionInitiale:
+      "Le vendeur a fait construire le bâtiment vendu",
+    situationBatimentConstructionInitiale10ans:
+      "La construction initiale a moins de 10 ans",
+    situationBatimentConstructionInitialeDommageOuvrage:
+      "Le vendeur a souscrit une assurance dommage-ouvrage",
+    situationBatimentTravauxPost:
+      "Des travaux postérieurs à la construction initiale ont été effectués par le vendeur",
+    situationBatimentTravauxPostAutorisation:
+      "Ces travaux postérieurs à la construction ont fait l'objet d'autorisations d'urbanisme",
   };
 
   const contratSurBienAnswer = {
     contratBienLibre: "Le bien sera libre le jour de la vente",
     contratLocationRecente: "Le bien a été loué précédemment",
-    contratAffichage: "Il existe un contrat d'affichage publicitaire sur le bien",
+    contratAffichage:
+      "Il existe un contrat d'affichage publicitaire sur le bien",
     contratDetecteurFumee: "Le bien est équipé d'un détecteur de fumée",
     contratAutres: "Il existe d'autres contrats sur le bien",
     contratAutresNom: "textArea",
-    contratEquipementGarantie: "Certains équipements font encore l'object de garanties",
+    contratEquipementGarantie:
+      "Certains équipements font encore l'object de garanties",
     contratEquipementNom: "textArea",
-    contratProcedure: "Le vendeur est partie à une procédure judicaire sur le bien vendu",
+    contratProcedure:
+      "Le vendeur est partie à une procédure judicaire sur le bien vendu",
   };
 
   const avantageFiscalAnswer = {
     avantageFiscal: "Le bien fait l'objet d'un avantage fiscal",
-    avantageFiscalNom: "textArea"
+    avantageFiscalNom: "textArea",
   };
 
   const chauffageAnswer = {
     chauffageCuveMazout: "Le bien possède une cuve à mazout",
     chauffageDeVille: "Le bien est raccordé au gaz de ville",
     chauffageCiterne: "Le bien est raccordé à une citerne à gaz",
-    chauffageCiterneProprietaire: "Le vendeur est propriétaire de la citerne à gaz",
+    chauffageCiterneProprietaire:
+      "Le vendeur est propriétaire de la citerne à gaz",
     chauffageCiterneLocataire: "Le vendeur est locataire de la citerne à gaz",
-    chauffageCiterneCoordonnee:"textArea",
+    chauffageCiterneCoordonnee: "textArea",
   };
 
   const pretAnswer = {
     pretHypotheque: "Le vendeur a souscrit une hypothèque sur le bien",
-    pretCreditRelais:"Le vendeur a souscrit un crédit-relais dans l'attente de la vente du bien",
-    pretProcedure:"Une procédure de saisie immobilière est en cours sur le bien",
-  }
+    pretCreditRelais:
+      "Le vendeur a souscrit un crédit-relais dans l'attente de la vente du bien",
+    pretProcedure:
+      "Une procédure de saisie immobilière est en cours sur le bien",
+  };
 
   const plusValuesAnswer = {
-    plusValuesResidencePrincipale: "Le bien constitue la résidence principale du vendeur",
-    plusValueRemploi: "Le vendeur envisage de remployer le prix de vente dans l'achat de sa résidence principale",
+    plusValuesResidencePrincipale:
+      "Le bien constitue la résidence principale du vendeur",
+    plusValueRemploi:
+      "Le vendeur envisage de remployer le prix de vente dans l'achat de sa résidence principale",
     plusValueRetraiteInvalide: "Le vendeur est retraité ou invalide",
     plusValueTravaux: "Le vendeur a effectué des travaux sur le bien",
-  }
+  };
 
   const sinistreAnswer = {
-    sinistreCatastropheNaturelle: "Le vendeur a été indemnisé suite à un sinistre reconnu comme catastrophe naturelle"
-  }
+    sinistreCatastropheNaturelle:
+      "Le vendeur a été indemnisé suite à un sinistre reconnu comme catastrophe naturelle",
+  };
 
   const displayAnswer = (answerObject) => {
+    // buffer array
     let displayArray = [];
-    for (const key in answerObject) {
-      if (dataState[key] === "true" || dataState[key] === true) {
-        displayArray.push(answerObject[key]);
+
+    const dataStateKeysArray = Object.keys(dataState);
+
+    // this part loop all the keys of dataState and see if they match the keys in answerObject
+    dataStateKeysArray.forEach((dataKeys) => {
+      if (keyExists(answerObject, dataKeys)) {
+        // if true : push answerObject correpondent value to displayArray
+        if (dataState[dataKeys] === "true" || dataState[dataKeys] === true) {
+          displayArray.push(answerObject[dataKeys]);
+        }
+
+        // this piece of code handle the "textArea" state
+        if (
+          dataState[dataKeys] !== "true" &&
+          dataState[dataKeys] !== true &&
+          dataState[dataKeys] !== "false" &&
+          dataState[dataKeys] !== false &&
+          dataState[dataKeys] !== undefined
+        ) {
+          displayArray.push(`${answerObject[dataKeys]} ${dataState[dataKeys]}`);
+        }
       }
-      if (
-        dataState[key] !== "true" ||
-        dataState[key] !== true ||
-        dataState[key] !== "false" ||
-        dataState[key] !== false ||
-        dataState[key] !== undefined
-        //FIXME: this piece of code doesn't work : must change the conditions
-      ) {
-        displayArray.push(answerObject[key] + dataState[key]);
-      }
-    }
+    });
     return displayArray.map((item, keys) => <div key={keys}>{item}</div>);
   };
 
