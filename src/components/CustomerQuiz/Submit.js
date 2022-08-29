095f3f9c345b8bcd627de6f68a9bdaa9f4fe5bbf
amaury <tissot.amaury@gmail.com>
2022-08-29 19:30:48 +0200

    Add comment

--- src/components/CustomerQuiz/Submit.js
+++ src/components/CustomerQuiz/Submit.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Submit button component : first props = the state value name / second props = the setState hook
 const Submit = ({ valueName, setState }) => {
   const handleClick = () => {
     setState(valueName);
