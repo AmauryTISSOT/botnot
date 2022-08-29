8eb064a39ca4451037a6c62c4d26a245ce1d384f
amaury <tissot.amaury@gmail.com>
2022-08-29 19:31:14 +0200

    Add submit button & CustomerQuizSummary

--- src/components/CustomerQuiz.js
+++ src/components/CustomerQuiz.js
@@ -9,24 +9,68 @@ import Pret from "./CustomerQuiz/Pret";
 import Servitude from "./CustomerQuiz/Servitude";
 import SituationBatiment from "./CustomerQuiz/SituationBatiment";
 import SituationTerrain from "./CustomerQuiz/SituationTerrain";
+import Submit from "./CustomerQuiz/Submit";
+import CustomerQuizSummary from "./CustomerQuiz/CustomerQuizSummary";
 
 const CustomerQuiz = () => {
+  // Customer quiz state hook
+  const [customerQuizState, setCustomerQuizState] = useState("");
 
-  const [customerQuizState, setCustomerQuizState] = useState("")
+  // Hook who handle the summary rendering
+  const [showSummary, setShowSummary] = useState(false);
 
   return (
-    <div className="App">
-      <SituationTerrain customerState={customerQuizState} setCustomerState={setCustomerQuizState} />
-      <EtatSolPollution customerState={customerQuizState} setCustomerState={setCustomerQuizState}/>
-      <Servitude customerState={customerQuizState} setCustomerState={setCustomerQuizState}/>
-      <Assainissement customerState={customerQuizState} setCustomerState={setCustomerQuizState}/>
-      <SituationBatiment customerState={customerQuizState} setCustomerState={setCustomerQuizState}/>
-      <ContratSurBien customerState={customerQuizState} setCustomerState={setCustomerQuizState}/>
-      <AvantageFiscal customerState={customerQuizState} setCustomerState={setCustomerQuizState}/>
-      <Chauffage customerState={customerQuizState} setCustomerState={setCustomerQuizState}/>
-      <Pret customerState={customerQuizState} setCustomerState={setCustomerQuizState}/>
-      <PlusValues customerState={customerQuizState} setCustomerState={setCustomerQuizState}/>
-    </div>
+    <>
+      {!showSummary ? (
+        <div className="App">
+          <SituationTerrain
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <EtatSolPollution
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <Servitude
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <Assainissement
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <SituationBatiment
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <ContratSurBien
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <AvantageFiscal
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <Chauffage
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <Pret
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <PlusValues
+            customerState={customerQuizState}
+            setCustomerState={setCustomerQuizState}
+          />
+          <Submit valueName={true} setState={setShowSummary} />
+        </div>
+      ) : (
+        <div className="App">
+          <CustomerQuizSummary dataState={customerQuizState} />
+        </div>
+      )}
+    </>
   );
 };
 
