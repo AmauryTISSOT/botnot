a2d8e6efdb7de342751057a7baffb28989408703
amaury <tissot.amaury@gmail.com>
2022-08-30 17:46:09 +0200

    Add integration test for new component

--- src/__test__/CustomerQuiz.test.js
+++ src/__test__/CustomerQuiz.test.js
@@ -31,4 +31,48 @@ describe("CustomerQuiz component integration testing", () => {
     const summaryElement = screen.getByText(/Situation du terrain/i);
     expect(summaryElement).toBeInTheDocument();
   });
+  test("Filling DossierName input should display the input value in CustomerQuizSummary", () => {
+    render(<CustomerQuiz />);
+    const inputDossierName = screen.getByTestId("dossierNom")
+    user.type(inputDossierName, "Vente 123")
+    const inputDossierReference = screen.getByTestId("dossierNom")
+    user.type(inputDossierReference, "201")
+    const buttonElement = screen.getByRole("button", { name: /submit/i });
+    fireEvent.click(buttonElement);
+    const dossierNameSummary = screen.getByText(/vente 123/i);
+    expect(dossierNameSummary).toBeInTheDocument();
+    const dossierReferenceSummary = screen.getByText(/201/i);
+    expect(dossierReferenceSummary).toBeInTheDocument();
+  });
+  test("Filling ClientName input should display the input value in CustomerQuizSummary", () => {
+    render(<CustomerQuiz />);
+    const inputClientName = screen.getByTestId("clientNom")
+    user.type(inputClientName, "Doe")
+    const inputClientPrenom = screen.getByTestId("clientPrenom")
+    user.type(inputClientPrenom, "John")
+    const buttonElement = screen.getByRole("button", { name: /submit/i });
+    fireEvent.click(buttonElement);
+    const dossierNameSummary = screen.getByText(/Doe/i);
+    expect(dossierNameSummary).toBeInTheDocument();
+    const dossierPrenomSummary = screen.getByText(/John/i);
+    expect(dossierPrenomSummary).toBeInTheDocument();
+  });
+  test("Filling AdresseDuBien input should display the input value in CustomerQuizSummary", () => {
+    render(<CustomerQuiz />);
+    const inputAdresse = screen.getByTestId("bienAdresse")
+    user.type(inputAdresse, "200 chemin du temple")
+    const inputCodePostal = screen.getByTestId("bienCodePostal")
+    user.type(inputCodePostal, "30100")
+    const inputCommune = screen.getByTestId("bienCommune")
+    user.type(inputCommune, "ALES")
+    const buttonElement = screen.getByRole("button", { name: /submit/i });
+    fireEvent.click(buttonElement);
+    const adresseSummary = screen.getByText(/200 chemin du temple/i);
+    expect(adresseSummary).toBeInTheDocument();
+    const codePostalSummary = screen.getByText(/30100/i);
+    expect(codePostalSummary).toBeInTheDocument();
+    const communeSummary = screen.getByText(/ALES/i);
+    expect(communeSummary).toBeInTheDocument();
+  });
+
 });
