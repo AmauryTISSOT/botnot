c85949ba9782e9c97a62c6c40d5a62ee2420152d
amaury <tissot.amaury@gmail.com>
2022-09-04 19:58:20 +0200

    Add submit button testing

--- src/__test__/AutoCompleteAdresse.test.js
+++ src/__test__/AutoCompleteAdresse.test.js
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
-import { findByText, fireEvent, getByText, render, screen, waitFor } from "@testing-library/react";
+import {
+  findByText,
+  fireEvent,
+  getByText,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import "@testing-library/jest-dom"; // optional
 import AutoCompleteAdresse from "../components/API/AutoCompleteAdresse";
 import user from "@testing-library/user-event";
 
+const MockComponent = () => {
+  const [mockState, setMockState] = useState("");
 
+  return (
+    <>
+      <AutoCompleteAdresse setState={setMockState} />
+      <div data-testid="stateDiv">adresse : {mockState.adresseName}</div>
+    </>
+  );
+};
 
 describe("AutoCompleteAdresse component unit test", () => {
   test("should render correctly", () => {
@@ -19,9 +35,33 @@ describe("AutoCompleteAdresse component unit test", () => {
 
   test("should correctly display api values", async () => {
     render(<AutoCompleteAdresse />);
-    const inputAdresse = screen.getByPlaceholderText(/Indiquer l'adresse ici.../i)
-    await user.type(inputAdresse, "55 de la rue du Faubourg-Saint-Honoré", {delay:100})
-    const valueElement = await screen.findByText(/55 Rue du Faubourg Saint-Honoré 75008 Paris/i)
+    const inputAdresse = screen.getByPlaceholderText(
+      /Indiquer l'adresse ici.../i
+    );
+    await user.type(inputAdresse, "55 de la rue du Faubourg-Saint-Honoré", {
+      delay: 100,
+    });
+    const valueElement = await screen.findByText(
+      /55 Rue du Faubourg Saint-Honoré 75008 Paris/i
+    );
     expect(valueElement).toBeInTheDocument();
+  }, 10000);
+
+  test("clicking on submit button should correctly send data to state", async () => {
+    render(<MockComponent />);
+    const inputAdresse = screen.getByPlaceholderText(
+      /Indiquer l'adresse ici.../i
+    );
+    await user.type(inputAdresse, "55 de la rue du Faubourg-Saint-Honoré", {
+      delay: 50,
+    });
+    const valueElement = await screen.findByText(
+      "55 Rue du Faubourg Saint-Honoré 75008 Paris"
+    );
+    fireEvent.click(valueElement);
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+    const stateValue = screen.getByText(/adresse : 55 /i)
+    expect(stateValue).toBeInTheDocument();
   }, 10000);
 });
