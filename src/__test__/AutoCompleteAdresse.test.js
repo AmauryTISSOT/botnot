fd6b525dab07bcffef8355d50e9de236c70c3cd0
amaury <tissot.amaury@gmail.com>
2022-09-04 19:25:55 +0200

    Add display api values test

--- src/__test__/AutoCompleteAdresse.test.js
+++ src/__test__/AutoCompleteAdresse.test.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { fireEvent, getByText, render, screen } from "@testing-library/react";
+import { findByText, fireEvent, getByText, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom"; // optional
 import AutoCompleteAdresse from "../components/API/AutoCompleteAdresse";
 import user from "@testing-library/user-event";
+
+
 
 describe("AutoCompleteAdresse component unit test", () => {
   test("should render correctly", () => {
@@ -15,12 +17,11 @@ describe("AutoCompleteAdresse component unit test", () => {
     expect(villeElement).toBeInTheDocument();
   });
 
-  test("input value should correctly display", () => {
+  test("should correctly display api values", async () => {
     render(<AutoCompleteAdresse />);
     const inputAdresse = screen.getByPlaceholderText(/Indiquer l'adresse ici.../i)
-    user.type(inputAdresse, "55 de la rue du Faubourg-Saint-Honoré")
-    const valueElement = screen.getByText(/55 de la rue du Faubourg-Saint-Honoré/i)
-    screen.debug()
+    await user.type(inputAdresse, "55 de la rue du Faubourg-Saint-Honoré", {delay:100})
+    const valueElement = await screen.findByText(/55 Rue du Faubourg Saint-Honoré 75008 Paris/i)
     expect(valueElement).toBeInTheDocument();
-  });
+  }, 10000);
 });
