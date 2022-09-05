611c8cdbd714386d4ac668e85c7f901a4473b34a
amaury <tissot.amaury@gmail.com>
2022-09-05 20:53:52 +0200

    Working on fetch function

--- src/components/API/CodeInsee.js
+++ src/components/API/CodeInsee.js
@@ -1,31 +1,60 @@
 import React, { useEffect, useState } from "react";
 
-const CodeInsee = ({codePostal}) => {
-    const [listeCommune, setListeCommune] = useState();
+const CodeInsee = () => {
+  const [listeCommune, setListeCommune] = useState();
+  const [input, setInput] = useState("");
 
+  useEffect(() => {
 
-    useEffect(() => {
-        fetch(`https://apicarto.ign.fr/api/codes-postaux/communes/${codePostal}`)
-          .then((response) => {
-            if (response.status === 200) return response.json();
-            else throw new Error("Invalid response");
-          })
-          .then((data) => {
-            // console.log(data)
-              setListeCommune([data])
-          })
-          .catch(error => setListeCommune(["invalid"]))
-      }, [codePostal]);
-    
-      console.log("liste",listeCommune)
+    //FIXME: need to copy the fetch function on autocomplete address
+    setTimeout(() => {
+      fetch(`https://apicarto.ign.fr/api/codes-postaux/communes/${input.postcode}`)
+        .then((response) => {
+          if (response.status === 200) return response.json();
+          else throw new Error("Invalid response");
+        })
+        .then((data) => {
+          // console.log(data)
+          setListeCommune([data]);
+        })
+        .catch((error) => setListeCommune(["invalid"]));
+    }, 200);
+  }, [input.postcode]);
 
+  console.log("input", input.postcode);
+  console.log(listeCommune)
 
+  const onFillHandler = (event) => {
+    setInput((prev) => ({
+      ...prev,
+      [event.target.id]: event.target.value,
+    }));
+  };
 
   return (
     <>
-    
-    </>
-  )
-}
+      <div>
+        <label>Code postal :</label>
+        <input
+          type="number"
+          data-testid="input"
+          id="postcode"
+          onChange={onFillHandler}
+        />
+      </div>
 
-export default CodeInsee
+      <label>Commune :</label>
+      <select>
+        <option value="">--Selectionner une commune--</option>
+        {/* {listeCommune !== undefined &&
+          listeCommune[0].map((item, keys) => (
+            <option key={keys} value={item.nomCommune}>
+              {item.nomCommune}
+            </option>
+          ))} */}
+      </select>
+    </>
+  );
+};
+
+export default CodeInsee;
