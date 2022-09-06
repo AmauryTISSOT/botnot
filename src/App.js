6ddbc7628a7f4ac71117e798ac2277d56ed8d134
amaury <tissot.amaury@gmail.com>
2022-09-06 21:03:11 +0200

    Add mock state for prod

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 import CodeInsee from './components/API/CodeInsee';
 
 function App() {
+
+  const [mockState, setMockState] = useState("")
+  console.log(mockState)
   return (
-    <CodeInsee/>
+    <CodeInsee setState={setMockState}/>
   );
 }
 
