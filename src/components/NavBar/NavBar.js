b4f07b6ba4011e5a163181e926e774ba199bbaf9
amaury <tissot.amaury@gmail.com>
2022-09-25 20:12:57 +0200

    Create NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = (props) => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+};
+
+export default NavBar;
