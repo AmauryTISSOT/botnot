d99dbcc9f733c444b2da7c6bb11c4ac2bd05c312
amaury <tissot.amaury@gmail.com>
2022-09-25 20:12:35 +0200

    Create Header test

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import user from "@testing-library/user-event";
+import Header from "./Header";
+
+describe("Header unit testing", () => {
+  test("should render correctly", () => {
+    render(<Header />);
+    const accueilElement = screen.getByText(/accueil/i);
+    expect(accueilElement).toBeInTheDocument();
+
+    const outilsElement = screen.getByText(/outils/i);
+    expect(outilsElement).toBeInTheDocument();
+
+    const aproposElement = screen.getByText(/a propos/i);
+    expect(aproposElement).toBeInTheDocument();
+
+    const logoElement = screen.getByText(/logo/i);
+    expect(logoElement).toBeInTheDocument()
+  });
+});
