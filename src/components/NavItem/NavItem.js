4194e353a0dcf691965127242bdc301e64a2cc5a
amaury <tissot.amaury@gmail.com>
2022-10-01 20:43:40 +0200

    Change <a> tag by <div>

--- src/components/NavItem/NavItem.js
+++ src/components/NavItem/NavItem.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-import "./NavItem.css"
+import "./NavItem.css";
 
 const NavItem = (props) => {
   const [open, setOpen] = useState(false);
 
   return (
     <li className="nav-item">
-      <a href="123" className="icon-button" onClick={() => setOpen(!open)}>
+      <div href="123" className="icon-button" onClick={() => setOpen(!open)}>
         {props.name}
-      </a>
-
+      </div>
       {open && props.children}
     </li>
   );
 };
 
 export default NavItem;
+
+
+//TODO: on click outside the menu => close the menu
