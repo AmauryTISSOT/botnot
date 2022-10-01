7ea1973904811ccad3ead0f77ab7529bb7a6473d
amaury <tissot.amaury@gmail.com>
2022-10-01 20:42:40 +0200

    Add child component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Header.css";
+import NavBar from "../NavBar/NavBar";
+import NavItem from "../NavItem/NavItem";
+import DropDownMenu from "../DropDownMenu/DropDownMenu";
 
 function Header() {
   return (
-    <>
-      <div>
-        LOGO
-      </div>
-    </>
+    <div className="header">
+      <div className="logo">BotNot</div>
+      <NavBar>
+        <NavItem name={"test1"} />
+        <NavItem name={"test2"}>
+          <DropDownMenu />
+        </NavItem>
+        <NavItem name={"test3"} />
+      </NavBar>
+    </div>
   );
 }
 
