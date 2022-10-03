318fbcd9b29a5c8efb2345184dd2024b769a0954
amaury <tissot.amaury@gmail.com>
2022-10-03 21:12:40 +0200

    Add Home and About routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-import Header from "./components/Header/Header";
+import {Routes, Route} from 'react-router-dom'
+import Home from "./components/Home./Home";
+import About from "./components/About/About";
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+      </Routes>
     </div>
   );
 }
