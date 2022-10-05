146c5e63fe1aa1e925cba6967b54febf3c519425
amaury <tissot.amaury@gmail.com>
2022-10-05 22:01:47 +0200

    Add Header component

--- src/NoMatch/NoMatch.js
+++ src/NoMatch/NoMatch.js
@@ -4,7 +4,7 @@ import Header from "../components/Header/Header";
 const NoMatch = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <h1>Error 404</h1>
       <div>The requested URL was not found on this server.</div>
     </>
