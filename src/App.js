aaeae62de41ef326f0b1260071218330939f1b81
amaury <tissot.amaury@gmail.com>
2022-10-05 21:28:18 +0200

    Fix misspell

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import {Routes, Route} from 'react-router-dom'
-import Home from "./components/Home./Home";
+import Home from "./components/Home/Home";
 import About from "./components/About/About";
 
 
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="about" element={<About/>}/>
+        
       </Routes>
     </div>
   );
