f36725745c878f0b39a164cdadfefa3c53685bcc
amaury <tissot.amaury@gmail.com>
2022-10-08 18:17:33 +0200

    Add new routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
-import NoMatch from "./NoMatch/NoMatch";
-
+import NoMatch from "./components/NoMatch/NoMatch";
+import Annuaire from "./components/Annuaire/Annuaire";
+import CustomerQuiz from "./components/CustomerQuiz";
+import Outils from "./components/Outils/Outils";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="*" element={<NoMatch/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+        <Route path="outils" element={<Outils />}>
+          <Route path="annuaire" element={<Annuaire />} />
+          <Route path="customerquiz" element={<CustomerQuiz />} />
+        </Route>
       </Routes>
     </div>
   );
