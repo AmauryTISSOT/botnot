52a486f0fb42d64ce8d73b8425033cc7139c5f3b
amaury <tissot.amaury@gmail.com>
2022-10-15 21:26:54 +0200

    import css

--- src/pages/Outils/Outils.js
+++ src/pages/Outils/Outils.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from "../../components/Header/Header";
+import "./Outils.css"
 
 const Outils = () => {
   return (
-    <div data-testid="outils">
+    <div data-testid="outils" className='outils-container'>
         <Header/>
         <Outlet/>
     </div>
