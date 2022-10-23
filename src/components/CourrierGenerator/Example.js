9d8c39f8474d0de2937b1dfe0fc67bc04eba40d4
amaury <tissot.amaury@gmail.com>
2022-10-23 18:19:57 +0200

    Update Example.js

--- src/components/CourrierGenerator/Example.js
+++ src/components/CourrierGenerator/Example.js
@@ -1,37 +1,37 @@
-import * as fs from "fs";
-import pkg from "docx";
+import { Document, Paragraph, TextRun } from "docx";
+import CourrierAdresse from "./CourrierComponent/CourrierAdresse";
+import CourrierDate from "./CourrierComponent/CourrierDate"
 
-
-const { Document, Packer, Paragraph, TextRun } = pkg
-
-// Documents contain sections, you can have multiple sections per document, go here to learn more about sections
-// This simple example will only contain one section
 const doc = new Document({
-    sections: [
-        {
-            properties: {},
-            children: [
-                new Paragraph({
-                    children: [
-                        new TextRun("Hello World"),
-                        new TextRun({
-                            text: "Foo Bar",
-                            bold: true,
-                        }),
-                        new TextRun({
-                            text: "\tGithub is the best",
-                            bold: true,
-                        }),
-                    ],
-                }),
-            ],
-        },
-    ],
+  sections: [
+    {
+      properties: {},
+      children: [
+        new Paragraph({
+          text: "Paragraph with spacing before",
+          spacing: {
+            before: 200,
+          },
+        }),
+        new Paragraph({
+          text: CourrierDate(),
+          spacing: {
+            before: 400,
+          },
+        }),
+
+        new Paragraph({
+          children: [
+            new TextRun({
+              text: "break",
+              break: 2,
+            }),
+          ],
+        }),
+        CourrierAdresse('test Adresse')
+      ],
+    },
+  ],
 });
 
-// Used to export the file into a .docx file
-Packer.toBuffer(doc).then((buffer) => {
-    fs.writeFileSync("My Document.docx", buffer);
-});
-
-// Done! A file called 'My Document.docx' will be in your file system.
+export default doc;
