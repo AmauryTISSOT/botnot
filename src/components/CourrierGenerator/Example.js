0e0214f12444b58e638c6298d7b59d1fa2b0a7b2
amaury <tissot.amaury@gmail.com>
2022-11-01 20:50:49 +0100

    work in progress

--- src/components/CourrierGenerator/Example.js
+++ src/components/CourrierGenerator/Example.js
@@ -1,45 +1,41 @@
-import { Document, Paragraph, TextRun, Header, Footer } from "docx";
+import { Document, Paragraph, TextRun, Header, Footer, Spacing } from "docx";
 import CourrierAdresse from "./CourrierComponent/CourrierAdresse";
 import CourrierDate from "./CourrierComponent/CourrierDate";
 import { convertMillimetersToTwip } from "docx";
+import MiseEnPage from "./CourrierComponent/MiseEnPage";
+import EmptySpacing from "./CourrierComponent/EmptySpacing";
+import FollowedBy from "./CourrierComponent/FollowedBy";
+import ClerkMail from "./CourrierComponent/ClerckMail";
+import CourrierObjet from "./CourrierComponent/CourrierObjet";
 
 const doc = new Document({
   sections: [
     {
-      properties: {
-        page: {
-          margin: {
-            top: convertMillimetersToTwip(38),
-            bottom: convertMillimetersToTwip(42.5),
-            left:convertMillimetersToTwip(50),
-            right: convertMillimetersToTwip(25),
-          },
-        },
-      },
-    children: [
-      new Paragraph({
-        indent: {left: convertMillimetersToTwip(62.4)},
-        children: [
-          new TextRun({
-            text: 'DIRECTION DU LOGEMENT ET DE L\'HABITAT',
-            bold: true,
-            font: 'Calibri',
-            size: 20,
-          })
-        ]
-      }),
-      new Paragraph({
-        indent: {left: convertMillimetersToTwip(62.4)},
-        children: [
-          new TextRun({
-            text: 'Sous-direction de l\'habitat',
-            font: 'Calibri',
-            size: 20,
-          })
-        ]
-      }),
-    ]  
-    
+      properties: MiseEnPage(38, 42.5, 50, 25),
+      children: [
+
+        CourrierAdresse(
+          "Direction du logement et de l'habitat \n 66 Bis Rue du Président Wilson \n 92300 LEVALOIS PERRET"
+        ),
+        EmptySpacing(),
+        CourrierDate(),
+        EmptySpacing(),
+        new Paragraph({
+          indent: { left: -convertMillimetersToTwip(20) },
+          children: [
+            new TextRun({
+              text: "Dossier suivi par :",
+              font: "Calibri",
+              size: 22,
+            }),
+          ],
+        }),
+        FollowedBy("Amaury Tissot"),
+        ClerkMail("email@paris.notaires.fr"),
+        CourrierObjet("TEST OBJET"),
+        EmptySpacing(),
+        EmptySpacing(),
+      ],
     },
   ],
 });
