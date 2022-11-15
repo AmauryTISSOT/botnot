fb4906f6e5aaa787b0a8e5f25723941a4b506afa
amaury <tissot.amaury@gmail.com>
2022-11-15 23:06:02 +0100

    Create CourrierHeader function

---
+++ src/components/CourrierGenerator/CourrierComponent/CourrierHeader.js
@@ -0,0 +1,34 @@
+import BlobImage from "src/utils/KeyExists/BlobImage.js";
+import HeaderImg from "../Images/Header.png";
+import { Header, Paragraph, ImageRun } from "docx";
+
+const CourrierHeader = () => {
+    return {
+        first: new Header({
+          children: [
+            // Header image
+            new Paragraph({
+              children: [
+                new ImageRun({
+                  data: BlobImage(HeaderImg),
+                  transformation: {
+                    width: 800,
+                    height: 234,
+                  },
+                  floating: {
+                    horizontalPosition: {
+                      offset: 0,
+                    },
+                    verticalPosition: {
+                      offset: 0,
+                    },
+                  },
+                }),
+              ],
+            }),
+          ],
+        }),
+      };
+};
+
+export default CourrierHeader();
