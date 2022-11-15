ce1d02c0d770a2c15e01d4278f2391c5a62a86fe
amaury <tissot.amaury@gmail.com>
2022-11-15 23:05:42 +0100

    Create CourrierFooter function

---
+++ src/components/CourrierGenerator/CourrierComponent/CourrierFooter.js
@@ -0,0 +1,34 @@
+import BlobImage from "src/utils/KeyExists/BlobImage.js";
+import FooterImg from "../Images/Footer.png";
+import { Footer, Paragraph, ImageRun } from "docx";
+
+const CourrierFooter = () => {
+    return {
+        first: new Footer({
+          children: [
+            // Footer image
+            new Paragraph({
+              children: [
+                new ImageRun({
+                  data: BlobImage(FooterImg),
+                  transformation: {
+                    width: 800,
+                    height: 143,
+                  },
+                  floating: {
+                    horizontalPosition: {
+                      offset: 0,
+                    },
+                    verticalPosition: {
+                      offset: 9180000,
+                    },
+                  },
+                }),
+              ],
+            }),
+          ],
+        }),
+      };
+}
+
+export default CourrierFooter;
