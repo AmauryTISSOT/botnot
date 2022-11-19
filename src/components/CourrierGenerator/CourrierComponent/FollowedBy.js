6efbea49f5845c8ea20dfe5b2c98f9080f9f67ea
amaury <tissot.amaury@gmail.com>
2022-11-19 22:32:19 +0100

    Add new paragraph with notary name

--- src/components/CourrierGenerator/CourrierComponent/FollowedBy.js
+++ src/components/CourrierGenerator/CourrierComponent/FollowedBy.js
@@ -1,18 +1,29 @@
 import { Paragraph, TextRun, convertMillimetersToTwip } from "docx";
 
 const FollowedBy = (ClerkName) => {
-  return new Paragraph({
-    indent: { left: -convertMillimetersToTwip(20) },
-
-    children: [
-      new TextRun({
-        text: ClerkName,
-        font: "Calibri",
-        size: 22, //font size 22 = 11 pt,
-        break: 1,
-      }),
-    ],
-  });
+  return [
+    new Paragraph({
+      indent: { left: -convertMillimetersToTwip(20) },
+      style: "classic",
+      children: [
+        new TextRun({
+          text: "Dossier suivi par :",
+          // font: "Calibri",
+          // size: 22,
+        }),
+      ],
+    }),
+    new Paragraph({
+      indent: { left: -convertMillimetersToTwip(20) },
+      style: "classic",
+      children: [
+        new TextRun({
+          text: ClerkName,
+          break: 1,
+        }),
+      ],
+    }),
+  ];
 };
 
 export default FollowedBy;
