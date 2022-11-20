a65dc937ab90bd9abfa714c149511251def8118d
amaury <tissot.amaury@gmail.com>
2022-11-20 21:01:53 +0100

    Create DTH data

---
+++ src/components/CourrierGenerator/CourrierComponent/CourrierData/CourrierData.js
@@ -0,0 +1,66 @@
+const DTH = [
+  {
+    text: "Madame, Monsieur,",
+    lineBreak: true,
+    tab: true,
+  },
+  {
+    text: "Je me permets de venir vers vous dans le cadre du dossier de vente cité en référence, portant sur un immeuble sis à PARIS (XX), XXXXXX.",
+    lineBreak: true,
+    tab: true,
+  },
+  {
+    text: "Aussi, vous saurais-je gré de bien vouloir me confirmer par retour de courrier les dispositions relatives à l’immeuble concernant :",
+    lineBreak: true,
+    tab: true,
+  },
+  {
+    text: "INSALUBRITE: ",
+    bold: true,
+    textRun: "Si l’immeuble fait l’objet de mesures d’insalubrité prises en application des articles 1331-22 et suivants du Code de la Santé Publique ;",
+    bulletPoint: true,
+    lineBreak: true,
+  },
+  {
+    text: "PERIL & INSECURITE: ",
+    bold: true,
+    textRun: "Si l’immeuble fait l’objet d’arrêtés municipaux pris en application des articles L511-1 à L511-7, R511-1 à R511-12, L129-1 à L129-7, et R129-1 à 129-9 du Code de la Construction et de l’Habitation ;",
+    bulletPoint: true,
+    lineBreak: true,
+  },
+  {
+    text: "EXPOSITION AU PLOMB & LUTTE CONTRE LE SATURNISME: ",
+    bold: true,
+    textRun: "Si l’immeuble fait l’objet de mesures de protection contre l’exposition au plomb ;",
+    bulletPoint: true,
+    lineBreak: true,
+  },
+  {
+    text: "LUTTE CONTRE LES TERMITES: ",
+    bold: true,
+    textRun: "Si l’immeuble est situé dans une zone contaminée par les termites ou susceptible de l’être en application des dispositions du Code de la Construction et de l’Habitation ;",
+    bulletPoint: true,
+    lineBreak: true,
+  },
+  {
+    text: "ASSAINISSEMENT: ",
+    bold: true,
+    textRun: "Si l’immeuble est raccordé au système d’assainissement de la ville de Paris ;",
+    bulletPoint: true,
+    lineBreak: true,
+  },
+  {
+    text: "RAVALEMENT: ",
+    bold: true,
+    textRun: "Si l’immeuble fait l’objet d’arrêtés municipaux pris en mesure de ravalement en application des articles 132-1 et suivants du Code de la Construction et de l’Habitation.",
+    bulletPoint: true,
+    lineBreak: true,
+  },
+  {
+    text: "Vous remerciant par avance de l’attention bienveillante que vous porterez à ma demande, je vous prie d'agréer, Madame, Monsieur, mes salutations distinguées.",
+    tab: true,
+    lineBreak: true,
+  },
+];
+
+export default DTH;
