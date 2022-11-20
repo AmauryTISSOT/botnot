44046c6d23eb19775ecf426d7ec5932c9c9c6df2
amaury <tissot.amaury@gmail.com>
2022-11-20 17:36:23 +0100

    Update for dev

--- src/components/CourrierGenerator/Example.js
+++ src/components/CourrierGenerator/Example.js
@@ -1,16 +1,46 @@
-import { Document } from "docx";
+import {
+  Document,
+  Table,
+  TableRow,
+  TableCell,
+  Paragraph,
+  ShadingType,
+  TextRun,
+} from "docx";
 
 import CourrierBoilerplate from "./CourrierComponent/CourrierBoilerplate";
+import CourrierTable from "./CourrierComponent/CourrierTable";
+
+const mockData = [
+
+  {
+    // prefixe: "001",
+    section: "AZ",
+    numero: "206",
+    lieudit: "LE BOIS",
+    surface: "00ha 01ca 23a",
+  }
+]
+ ;
+
+const table = CourrierTable(mockData);
 
 const doc = new Document(
-  CourrierBoilerplate(
-    "adresse",
-    "nom du clerc",
-    "mail du clerc",
-    "objet du courrier",
-    "XXXXX",
-    [{ text: "text du body" }]
-  )
+  // CourrierBoilerplate(
+  //   "adresse",
+  //   "nom du clerc",
+  //   "mail du clerc",
+  //   "objet du courrier",
+  //   "XXXXX",
+  //   [{ text: "text du body" }]
+  // )
+  {
+    sections: [
+      {
+        children: [table],
+      },
+    ],
+  }
 );
 
 export default doc;
