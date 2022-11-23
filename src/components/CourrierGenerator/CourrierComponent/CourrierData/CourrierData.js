ca802166827d7e26870787e34252bccc67c06a59
amaury <tissot.amaury@gmail.com>
2022-11-23 22:14:38 +0100

    Add new courrier data

--- src/components/CourrierGenerator/CourrierComponent/CourrierData/CourrierData.js
+++ src/components/CourrierGenerator/CourrierComponent/CourrierData/CourrierData.js
@@ -1,4 +1,4 @@
-const DTH = [
+export const DTH = [
   {
     text: "Madame, Monsieur,",
     lineBreak: true,
@@ -63,4 +63,229 @@ const DTH = [
   },
 ];
 
-export default DTH;
+export const demandeCU = [
+	{text: "LR avec AR : ",
+	underline : true,
+	bold: true, 
+	},
+	{text: "Madame, Monsieur",
+	tab: true, 
+	lineBreak: true,},
+	{text: "Je vous adresse sous ce pli une demande de certificat d’urbanisme relative à un immeuble situé à l’adresse suivante : ",
+	tab: true,
+	lineBreak: true,
+	textRun : "adresseBien",
+	},
+	{text: "tableauCadastre",
+	tab: true,
+	lineBreak: true,
+	},
+	{text: "Vous voudrez bien m’accuser réception de ce document.",
+	tab: true,
+	lineBreak: true,
+	},
+	{text: "Veuillez croire, Madame, Monsieur, à l’assurance de ma sincère considération.",
+	tab: true,
+	lineBreak: true,
+	},
+];
+
+
+export const demandeDia = [
+	{
+	text : "Madame, Monsieur,",
+	tab: true,
+	lineBreak: true,
+},
+	{
+	text : "Je vous adresse sous ce pli une déclaration d’intention d’aliéner concernant l'immeuble sis à l'adresse suivante : ",
+	tab: true,
+	lineBreak: true,
+	textRun : "adresseBien",
+},
+	{
+	text : "Cadastré : ",
+	tab: true,
+	lineBreak: true,
+},
+	{
+	text : "tableauCadastre",
+	tab: true,
+	lineBreak: true,
+},
+	{
+	text : "Appartenant à :",
+	tab: true,
+	lineBreak: true,
+	textRun : "nomVendeur",
+},
+	{
+	text : "Veuillez croire, Madame, Monsieur, à l’assurance de ma sincère considération.",
+	tab: true,
+	lineBreak: true,
+},
+];
+
+export const arretePeril = [
+	{
+	text: "Madame, Monsieur,",
+	tab: true,
+	lineBreak: true,
+		},
+	{
+	text: "Je vous remercie de m’informer si un arrêté de péril grève, ou est en passe de grever, l’immeuble appartenant à ",
+	tab: true,
+	lineBreak: true,
+	textRun : "nomVendeur",
+		},
+	{
+	text: "Situé à",
+	tab: true,
+	textRun : "adresseBien",
+	},
+	{
+	text: "Cadastré",
+	tab: true,
+	lineBreak: true,
+		},
+	{
+	text: "tableauCadastre",
+	tab: true,
+	lineBreak: true,
+		},
+	{
+	text: "Dans l’affirmative et s’il est établi, un état des mesures ordonnées me sera nécessaire.",
+	tab: true,
+	lineBreak: true,
+		},
+	{
+	text: "Vous trouverez ci-joint un extrait de plan cadastral.",
+	tab: true,
+	lineBreak: true,
+		},
+	{
+	text: "Veuillez croire, Madame, Monsieur, à l’assurance de ma sincère considération.",
+	tab: true,
+	lineBreak: true,
+		},
+];
+
+export const arreteAlignement = [
+	{
+	text: "Madame, Monsieur,",
+	tab: true,
+	lineBreak: true
+	},
+	{
+	text: "Je vous remercie de m’adresser un arrêté d'alignement faisant connaître les servitudes pouvant grever l’immeuble : ",
+	tab: true,
+	lineBreak: true,
+	textRun : "adresseBien",
+	},
+	{
+	text: "Cadastré",
+	tab: true,
+	lineBreak: true
+	},
+	{
+	text: "tableauCadastre",
+	tab: true,
+	lineBreak: true
+	},
+	{
+	text: "Appartenant à ",
+	tab: true,
+	lineBreak: true,
+	textRun: "nomVendeur",
+	},
+	{
+	text: "Vous trouverez ci-joint un extrait de plan cadastral.",
+	tab: true,
+	lineBreak: true
+	},
+	{
+	text: "Veuillez croire, Madame, Monsieur, à l’assurance de ma sincère considération.",
+	tab: true,
+	lineBreak: true
+	},
+];
+
+export const certificatNumerotage = [
+	{
+	text: "Madame, Monsieur,",
+	tab: true,
+	lineBreak: true,
+	},
+	{
+	text: "Je vous remercie de m’adresser un certificat de numérotage, concernant l’immeuble situé à : ",
+	tab: true,
+	lineBreak: true,
+	textRun : "adresseBien",
+	},
+	{
+	text: "Cadastré",
+	tab: true,
+	lineBreak: true,
+	},
+	{
+	text: "tableauCadastre",
+	tab: true,
+	lineBreak: true,
+	},
+	{
+	text: "Appartenant à ",
+	tab: true,
+	lineBreak: true,
+	textRun: "nomVendeur",
+	},
+	{
+	text: "Vous trouverez ci-joint un extrait de plan cadastral.",
+	tab: true,
+	lineBreak: true,
+	},
+	{
+	text: "Veuillez croire, Madame, Monsieur, à l’assurance de ma sincère considération.",
+	tab: true,
+	lineBreak: true,
+	},
+];
+
+export const certificatHygiene = [
+	{
+	text: "Madame, Monsieur,",
+	tab: true,
+	linebreak: true,
+	},
+	{
+	text: "L'office notarial étant chargé du dossier en référence, je vous remercie de m’adresser un certificat d’HYGIENE et de SALUBRITE concernant l’immeuble situé à : ",
+	tab: true,
+	linebreak: true,
+	textRun : "adresseBien",
+	},
+	{
+	text: "Cadastré",
+	tab: true,
+	linebreak: true,
+	},
+	{
+	text: "tableauCadastre",
+	tab: true,
+	linebreak: true,
+	},
+	{
+	text: "Appartenant à ",
+	tab: true,
+	linebreak: true,
+	textRun : "nomVendeur",
+	},
+	{
+	text: "Vous trouverez ci-joint un extrait de plan cadastral.",
+	tab: true,
+	linebreak: true,
+	},
+	{
+	text: "Veuillez croire, Madame, Monsieur, à l’assurance de ma sincère considération.",
+	tab: true,
+	linebreak: true,
+	},
+];
