62a1f4239323c26390a43b8b66c5b94a1eff15c5
amaury <tissot.amaury@gmail.com>
2022-12-10 17:04:56 +0100

    Create CompetenceNotaire component

---
+++ src/components/competenceNotaire/CompetenceNotaire.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe('competenceNotaire unit testing', () => { 
+
+    test('should first render correctly', () => { 
+        
+     })
+
+ })
