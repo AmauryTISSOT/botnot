98e06e5b4ca55ea103e1c5769f0d06fdfadb8f03
amaury <tissot.amaury@gmail.com>
2022-12-11 18:44:38 +0100

    Add rendering test

--- src/components/competenceNotaire/CompetenceNotaire.test.js
+++ src/components/competenceNotaire/CompetenceNotaire.test.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import CompetenceNotaire from "./CompetenceNotaire";
 
 describe('competenceNotaire unit testing', () => { 
 
     test('should first render correctly', () => { 
-        
-     })
+        render(<CompetenceNotaire/>)
+        const inputRadioElement = screen.getAllByRole("radio")
+        expect(inputRadioElement).toHaveLength(2)
+
+        const selectFormElement = screen.getAllByRole('combobox')
+        expect(selectFormElement).toHaveLength(2)
+     });
 
  })
