5d8bc64b1e5a342fb3c5372d91bbb91adbeac561
amaury <tissot.amaury@gmail.com>
2023-01-03 23:03:44 +0100

    Add beforeAll and change testing object

--- src/components/EmailGenerator/EmailSelect/EmailSelect.test.js
+++ src/components/EmailGenerator/EmailSelect/EmailSelect.test.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { fireEvent, render, screen, createEvent } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  screen,
+  cleanup,
+} from "@testing-library/react";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
 import EmailSelect from "./EmailSelect";
+
+beforeAll(cleanup);
 
 describe("EmailSelect unit testing", () => {
   test("should render correctly", () => {
@@ -67,7 +73,7 @@ describe("EmailSelect intergration testing", () => {
         type: "Title1",
         query: [
           {
-            value: "value1",
+            value: "garantie",
             question: "question1",
           },
         ],
