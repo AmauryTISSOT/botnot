88f944474c98e7a093ca6e946d96d990930ac7ac
amaury <tissot.amaury@gmail.com>
2023-01-04 22:08:23 +0100

    Create EmailData

---
+++ src/components/EmailGenerator/EmailData.js
@@ -0,0 +1,23 @@
+const EmailData = [{
+    test : {
+        input : [
+            {
+                type: "text",
+                id: "test-1",
+                label: "testing label-1",
+                placeholder: "0,00 €"
+            },
+            {
+                type: "text",
+                id: "test-2",
+                label: "testing label-2",
+                placeholder: "500,00 €"
+            },
+            {
+                type: "radio-yes-no",
+                id: "test-3",
+                label: "testing label-3",
+            }
+        ]
+    }
+}]
