fb5f47652a76d8ee9e72f2b4a8ef91e910beeaac
amaury <tissot.amaury@gmail.com>
2023-01-05 23:26:07 +0100

    Add new value

--- src/components/EmailGenerator/EmailData.js
+++ src/components/EmailGenerator/EmailData.js
@@ -1,23 +1,29 @@
-const EmailData = [{
-    test : {
-        input : [
-            {
-                type: "text",
-                id: "test-1",
-                label: "testing label-1",
-                placeholder: "0,00 €"
-            },
-            {
-                type: "text",
-                id: "test-2",
-                label: "testing label-2",
-                placeholder: "500,00 €"
-            },
-            {
-                type: "radio-yes-no",
-                id: "test-3",
-                label: "testing label-3",
-            }
-        ]
-    }
-}]
+const EmailData = {
+  test: {
+    input: [
+      {
+        type: "text",
+        id: "test1",
+        label: "testing label-1",
+        placeholder: "0,00 €",
+      },
+      {
+        type: "text",
+        id: "test2",
+        label: "testing label-2",
+        placeholder: "500,00 €",
+      },
+      {
+        type: "radio",
+        id: "test3",
+        label: "testing label-3",
+        value: ["oui", "non"]
+      },
+    ],
+    mailString:
+      "This is a test. The variable 1 is {{test1}}. The variable 2 is {{test2}}. The variable 3 is {{test3}} ",
+  },
+
+};
+
+export default EmailData;
