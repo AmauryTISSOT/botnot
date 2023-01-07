e29c7ba6412da2de5f5372e20b68dba440457b5c
amaury <tissot.amaury@gmail.com>
2023-01-07 23:13:06 +0100

    Create EmailList

---
+++ src/components/EmailGenerator/EmailList.js
@@ -0,0 +1,17 @@
+const emailListData = [
+  {
+    type: "test",
+    query: [
+      {
+        value: "test",
+        question: "question1",
+      },
+      {
+        value: "test",
+        question: "question2",
+      },
+    ],
+  },
+];
+
+export default emailListData;
