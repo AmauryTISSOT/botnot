05b17c54b61f86f0c369d2cd9eb8b79b716f0212
amaury <tissot.amaury@gmail.com>
2023-01-07 20:52:59 +0100

    Rework test

--- src/components/EmailGenerator/EmailVariable/EmailVariable.test.js
+++ src/components/EmailGenerator/EmailVariable/EmailVariable.test.js
@@ -6,50 +6,34 @@ import userEvent from "@testing-library/user-event";
 
 describe("EmailVariable unit testing", () => {
   test("should render correctly", () => {
-    render(<EmailVariable emailValue={"garantie"} />);
-    const depotTestId = screen.getByTestId(/depot/i);
-    expect(depotTestId).toBeInTheDocument();
+    render(<EmailVariable emailValue={"test"} />);
 
-    const provisionTestId = screen.getByTestId(/provision/i);
-    expect(provisionTestId).toBeInTheDocument();
+    const textInputElement = screen.getByTestId("test-test1");
+    expect(textInputElement).toBeInTheDocument();
 
-    const pretTestId = screen.getByTestId("pret");
-    expect(pretTestId).toBeInTheDocument();
+    const placeholderElement = screen.getByPlaceholderText("0,00 €")
+    expect(placeholderElement).toBeInTheDocument();
 
-    const sruTestId = screen.getByTestId("sru");
-    expect(sruTestId).toBeInTheDocument();
+    const radioInputElement = screen.getByTestId("test-test3-oui");
+    expect(radioInputElement).toBeInTheDocument();
 
-    const instrumentaireTestId = screen.getByTestId("instrumentaire");
-    expect(instrumentaireTestId).toBeInTheDocument();
+    const selectInputElement = screen.getByTestId("test-test4-option one")
+    expect(selectInputElement).toBeInTheDocument();
   });
 
   test("input should work correctly", () => {
-    render(<EmailVariable emailValue={"garantie"} />);
+    render(<EmailVariable emailValue={"test"} />);
 
-    const depotInput = screen.getByPlaceholderText("0,00 €");
-    fireEvent.change(depotInput, { target: { value: "10000" } });
-    expect(depotInput.value).toBe("10000");
+    const textInputElement = screen.getByPlaceholderText("0,00 €");
+    fireEvent.change(textInputElement, { target: { value: "10000" } });
+    expect(textInputElement.value).toBe("10000");
 
-    const provisionInput = screen.getByPlaceholderText("500,00 €");
-    fireEvent.change(provisionInput, { target: { value: "500" } });
-    expect(provisionInput.value).toBe("500");
+    const radioInputElement = screen.getByTestId("test-test3-oui")
+    fireEvent.click(radioInputElement);
+    expect(radioInputElement).toBeChecked();
 
-    const pretTestId = screen.getByTestId("pret");
-    userEvent.selectOptions(pretTestId, "oui");
-    expect(screen.getByTestId("pretOui").selected).toBe(true);
-    userEvent.selectOptions(pretTestId, "non");
-    expect(screen.getByTestId("pretOui").selected).toBe(false);
-
-    const sruTestId = screen.getByTestId("pret");
-    userEvent.selectOptions(sruTestId, "oui");
-    expect(screen.getByTestId("pretOui").selected).toBe(true);
-    userEvent.selectOptions(sruTestId, "non");
-    expect(screen.getByTestId("pretOui").selected).toBe(false);
-
-    const instrumentaireTestId = screen.getByTestId("instrumentaire");
-    userEvent.selectOptions(instrumentaireTestId, "oui");
-    expect(screen.getByTestId("instrumentaireOui").selected).toBe(true);
-    userEvent.selectOptions(instrumentaireTestId, "non");
-    expect(screen.getByTestId("instrumentaireOui").selected).toBe(false);
+    const selectInputElement = screen.getByTestId("test-test4");
+    userEvent.selectOptions(selectInputElement, "option one");
+    expect(screen.getByTestId("test-test4-option one").selected).toBe(true);
   });
 });
