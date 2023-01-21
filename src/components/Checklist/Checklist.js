dbda3ab9b4610a8d8b345f7d47917a331b6a10ef
amaury <tissot.amaury@gmail.com>
2023-01-21 20:03:47 +0100

    Add Checklist CSS

--- src/components/Checklist/Checklist.js
+++ src/components/Checklist/Checklist.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Checklist.css"
 
 const Checklist = ({data}) => {
   const [checklistItems, setChecklistItems] = useState(data);
@@ -15,11 +16,12 @@ const Checklist = ({data}) => {
   };
 
   return (
-    <div>
-      <ul>
+    <div className="checklist-wrap">
+      <ul className="checklist-container">
         {checklistItems.map((item) => (
-          <li key={item.id}>
+          <li key={item.id} className="checklist-list">
             <input
+              className="checkbox"
               type="checkbox"
               onClick={() => handleCheckboxClick(item.id)}
             />
@@ -32,6 +34,12 @@ const Checklist = ({data}) => {
           </li>
         ))}
       </ul>
+      <button
+        className="retour-button"
+        onClick={() => window.location.reload()}
+      >
+        Retour
+      </button>
     </div>
   );
 };
