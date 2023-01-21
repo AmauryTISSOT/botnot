5a05388bcdcf5e0cb49670f45a2019b465ed26f1
amaury <tissot.amaury@gmail.com>
2023-01-21 19:06:58 +0100

    Create checklist data

---
+++ src/components/Checklist/ChecklistData/ChecklistData.js
@@ -0,0 +1,107 @@
+const checkListData = {
+  test: [
+    { id: 0, text: "Item 1", checked: false },
+    { id: 1, text: "Item 2", checked: false },
+    { id: 2, text: "Item 3", checked: false },
+  ],
+  dia: [
+    { id: 0, text: "Générer le courrier d'accompagnement", checked: false },
+    { id: 1, text: "Générer la DIA", checked: false },
+    {
+      id: 2,
+      text: "Sur le courrier d'accompagnement : indiquer le numéro du recommandé",
+      checked: false,
+    },
+    { id: 3, text: "Compléter la DIA", checked: false },
+    { id: 4, text: "Vérifier les points complétés", checked: false },
+    {
+      id: 5,
+      text: "Imprimer les pages 1 à 3 en 4 exemplaires",
+      checked: false,
+    },
+    {
+      id: 6,
+      text: "Imprimer le courrier d'accompagnement en 1 seul exemplaire",
+      checked: false,
+    },
+    {
+      id: 7,
+      text: "Signer + tampon sur le courrier d'accompagnement",
+      checked: false,
+    },
+    { id: 8, text: "Signer + tampon sur les DIA", checked: false },
+    {
+      id: 9,
+      text: "Scanner le courrier d'accompagner + 1 exemplaire de DIA",
+      checked: false,
+    },
+    {
+      id: 10,
+      text: "Rattacher le scan au dossier informatique",
+      checked: false,
+    },
+    {
+      id: 11,
+      text: "Vérifier que les 4 exemplaires sont complets et ont été signés",
+      checked: false,
+    },
+    { id: 12, text: "Envoyer le courrier", checked: false },
+  ],
+  cu: [
+    { id: 0, text: "Générer le courrier d’accompagnement", checked: false },
+    { id: 1, text: "Générer la demande de CU", checked: false },
+    {
+      id: 2,
+      text: "Sur le courrier d’accompagnement : indiquer le numéro de recommandé",
+      checked: false,
+    },
+    { id: 3, text: "Compléter le certificat d’urbanisme", checked: false },
+    { id: 4, text: "Vérifier les points complétés", checked: false },
+    { id: 5, text: "Obtenir une copie du plan cadastral", checked: false },
+    {
+      id: 6,
+      text: "Imprimer le plan cadastral en 2 exemplaires",
+      checked: false,
+    },
+    {
+      id: 7,
+      text: "Imprimer la demande de CU en 2 exemplaires",
+      checked: false,
+    },
+    {
+      id: 8,
+      text: "Imprimer le courrier d’accompagnement en 1 exemplaire",
+      checked: false,
+    },
+    {
+      id: 9,
+      text: "Signer + tampon le courrier d’accompagnement",
+      checked: false,
+    },
+    {
+      id: 10,
+      text: "Signer + tampon le courrier d’accompagnement",
+      checked: false,
+    },
+    { id: 11, text: "Signer + tampon de la demande de CU", checked: false },
+    {
+      id: 12,
+      text: "Scanner le courrier d’accompagnement + la demande de CU ",
+      checked: false,
+    },
+    {
+      id: 13,
+      text: "Rattacher le scan au dossier informatique",
+      checked: false,
+    },
+    {
+      id: 14,
+      text: "Vérifier que les 2 exemplaires sont complets et ont été signés",
+      checked: false,
+    },
+    { id: 15, text: "Envoyer le courrier", checked: false },
+    { id: 16, text: "", checked: false },
+  ],
+};
+
+export default checkListData;
