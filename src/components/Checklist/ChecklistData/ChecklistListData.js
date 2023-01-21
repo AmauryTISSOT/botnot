5c6074c920f6cf0b307bf9ca4eab350fe1174ee7
amaury <tissot.amaury@gmail.com>
2023-01-21 19:07:15 +0100

    Create checklist list data

---
+++ src/components/Checklist/ChecklistData/ChecklistListData.js
@@ -0,0 +1,17 @@
+const checklistList = [
+  {
+    type: "Urbanisme",
+    query: [
+      {
+        value: "dia",
+        question: "Déclaration d'intention d'aliéner (DIA)",
+      },
+      {
+        value: "cu",
+        question: "Certificat d'urbanisme (CU)",
+      },
+    ],
+  },
+];
+
+export default checklistList;
