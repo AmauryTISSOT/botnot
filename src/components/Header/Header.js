d16a281253c478e54d3281ba6318ad8dbe02395b
amaury <tissot.amaury@gmail.com>
2023-01-22 18:12:31 +0100

    Add darkmode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,23 @@ import NavBar from "./NavBar/NavBar";
 import NavItem from "./NavItem/NavItem";
 import DropDownMenu from "./DropDownMenu/DropDownMenu";
 import { Link } from "react-router-dom";
+import Darkmode from "./DarkModeButton/Darkmode";
 
 function Header() {
   return (
     <div className="header">
-      <Link className="logo" to="/">BotNot</Link>
+      <Link className="logo" to="/">
+        BotNot <i className="version"> Alpha v.1</i>
+      </Link>
       <NavBar>
-        <NavItem name={"Accueil"} to="/" />
-        <NavItem name={"Outils"}>
-          <DropDownMenu />
-        </NavItem>
-        <NavItem name={"A propos"} to="/about" />
+        <div className="header-menu-container">
+          <NavItem name={"Accueil"} to="/" />
+          <NavItem name={"Outils"}>
+            <DropDownMenu />
+          </NavItem>
+          <NavItem name={"A propos"} to="/about" />
+        </div>
+        <Darkmode />
       </NavBar>
     </div>
   );
