5906a251c22942bf96af5ff11aebd4a849a74775
amaury <tissot.amaury@gmail.com>
2023-01-29 18:39:17 +0100

    Fix test with MemoryRouter

--- src/components/Header/DropDownMenu/DropDownMenu.test.js
+++ src/components/Header/DropDownMenu/DropDownMenu.test.js
@@ -2,26 +2,38 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import DropDownMenu from "./DropDownMenu";
+import { MemoryRouter } from "react-router-dom";
 
 describe("DropDownMenu unit testing", () => {
   test("main menu should render correctly", () => {
-    render(<DropDownMenu/>);
+    render(
+      <MemoryRouter>
+        <DropDownMenu />
+      </MemoryRouter>
+    );
     const testElement = screen.getByTestId(/main/i);
     expect(testElement).toBeInTheDocument();
   });
 
   test("menu1 should not render", () => {
-    render(<DropDownMenu/>);
+    render(
+      <MemoryRouter>
+        <DropDownMenu />
+      </MemoryRouter>
+    );
     const testElement = screen.queryByTestId(/menu1/i);
     expect(testElement).not.toBeInTheDocument();
   });
 
   test("menu2 should not render", () => {
-    render(<DropDownMenu/>);
+    render(
+      <MemoryRouter>
+        <DropDownMenu />
+      </MemoryRouter>
+    );
     const testElement = screen.queryByTestId(/menu2/i);
     expect(testElement).not.toBeInTheDocument();
   });
-
 });
 
-//TODO: integration testing 
+//TODO: integration testing
