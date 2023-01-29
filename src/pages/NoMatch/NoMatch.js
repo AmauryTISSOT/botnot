86fef12d0e37bead1678d48f586d27008723a1fb
amaury <tissot.amaury@gmail.com>
2023-01-29 20:27:49 +0100

    Add Link to go back to home

--- src/pages/NoMatch/NoMatch.js
+++ src/pages/NoMatch/NoMatch.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./NoMatch.css"
 
 const NoMatch = () => {
   return (
-    <>
+    <div className="backgroundNoMatch">
       <h1>Error 404</h1>
       <div>The requested URL was not found on this server.</div>
-    </>
+      <Link className="backtohome" to={"/"}>Get back to home</Link>
+    </div>
   );
 };
 
