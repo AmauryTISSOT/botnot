77ffc8b84082af5ccfccf21616b8755486f0c959
amaury <tissot.amaury@gmail.com>
2023-01-29 19:50:19 +0100

    Add Link and refractor

--- src/components/Checklist/ChecklistSelect/ChecklistSelect.js
+++ src/components/Checklist/ChecklistSelect/ChecklistSelect.js
@@ -1,51 +1,30 @@
-import React, { useState } from "react";
-import Checklist from "../Checklist";
-import checkListData from "../ChecklistData/ChecklistData";
+import { Link } from "react-router-dom";
+import checklistList from "../ChecklistData/ChecklistListData";
 
-const ChecklistSelect = ({ checklistList }) => {
-  const [checklistSelected, setEmailSelected] = useState("");
-  const [checklistClicked, setQuestionClicked] = useState(false);
-
-  const clickHandler = (event) => {
-    setEmailSelected(event.target.id);
-    setQuestionClicked(true);
-  };
-
-  const checklistSelector = () => {
-    return (
-      <>
-        <h1>Sélectionner un type de checklist</h1>
-        <div className="question-container">
-          {Object.values(checklistList).map((checklist) => (
-            <div className="question-list" key={checklist.type.toString()}>
-              <h2>{checklist.type}</h2>
-              <ul className="question-link-list">
-                {checklist.query.map((checklist1, number1) => (
-                  <li
-                    key={number1}
-                    id={checklist1.value}
-                    data-testid="question-element"
-                    onClick={clickHandler}
-                    className="email-link"
-                  >
-                    {checklist1.question}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      </>
-    );
-  };
-
+const ChecklistSelect = () => {
   return (
     <>
-      {checklistClicked ? (
-        <Checklist data={checkListData[checklistSelected]} />
-      ) : (
-        checklistSelector()
-      )}
+      <h1>Sélectionner un type de checklist</h1>
+      <div className="question-container">
+        {Object.values(checklistList).map((checklist) => (
+          <div className="question-list" key={checklist.type.toString()}>
+            <h2>{checklist.type}</h2>
+            <ul className="question-link-list">
+              {checklist.query.map((checklist1, number1) => (
+                <Link
+                  key={number1}
+                  id={checklist1.value}
+                  data-testid="question-element"
+                  className="email-link"
+                  to={`/outils/checklistGenerator/${checklist1.value}`}
+                >
+                  {checklist1.question}
+                </Link>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
