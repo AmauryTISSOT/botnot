4a373a97fd7b208fcb825fd1fb8ed1210754dc13
amaury <tissot.amaury@gmail.com>
2023-01-29 19:50:37 +0100

    Remove Header component

--- src/pages/NoMatch/NoMatch.js
+++ src/pages/NoMatch/NoMatch.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "../../components/Header/Header";
 
 const NoMatch = () => {
   return (
     <>
-      <Header />
       <h1>Error 404</h1>
       <div>The requested URL was not found on this server.</div>
     </>
