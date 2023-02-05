edff7b25b6bbbeff722db1c8a32721d405fc01b9
amaury <tissot.amaury@gmail.com>
2023-02-05 19:45:28 +0100

    Create courrierListData

---
+++ src/components/CourrierGenerator/CourrierComponent/CourrierData/CourrierListData.js
@@ -0,0 +1,17 @@
+const courrierListData = [
+  {
+    type: "Title1",
+    query: [
+      {
+        value: "value1",
+        question: "courrier1",
+      },
+      {
+        value: "value2",
+        question: "courrier2",
+      },
+    ],
+  },
+];
+
+export default courrierListData;
