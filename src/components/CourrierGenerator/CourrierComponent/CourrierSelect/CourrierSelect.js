b9b400c55a2e44b5462ac92f4a9270f4dc4de52a
amaury <tissot.amaury@gmail.com>
2023-02-05 19:46:33 +0100

    Rework component for reactRouter handling

--- src/components/CourrierGenerator/CourrierComponent/CourrierSelect/CourrierSelect.js
+++ src/components/CourrierGenerator/CourrierComponent/CourrierSelect/CourrierSelect.js
@@ -1,52 +1,30 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import GenerateDocs from "../../GenerateDocs/GenerateDocs";
-import "./CourrierSelect.css";
+import courrierListData from "../CourrierData/CourrierListData";
 
-// data must be an array;
-const CourrierSelect = ({ data }) => {
-  const [courrierSelected, setCourrierSelected] = useState("");
-  const [courrierClicked, setCourrierClicked] = useState(false);
-
-  const clickHandler = (event) => {
-    setCourrierSelected(event.target.id);
-    setCourrierClicked(true);
-  };
-
-  const courrierSelector = () => {
-    return (
-      <>
-        <h1>Sélectionner un modèle de courrier</h1>
-        <div>
-          {Object.values(data).map((courrier) => (
-            <div key={courrier.type.toString()}>
-              <h2>{courrier.type}</h2>
-              <ul>
-                {courrier.query.map((courrier2, number1) => (
-                  <li
-                    key={number1}
-                    id={courrier2.value}
-                    data-testid="courrier-element"
-                    onClick={clickHandler}
-                  >
-                    {courrier2.question}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      </>
-    );
-  };
-
+const CourrierSelect = () => {
   return (
     <>
-      {courrierClicked ? (
-        <GenerateDocs value={courrierSelected} />
-      ) : (
-        courrierSelector()
-      )}
+      <h1>Sélectionner un type de courrier</h1>
+      <div className="question-container">
+        {Object.values(courrierListData).map((courrier) => (
+          <div className="question-list" key={courrier.type.toString()}>
+            <h2>{courrier.type}</h2>
+            <ul className="question-link-list">
+              {courrier.query.map((courrier1, number1) => (
+                <Link
+                  key={number1}
+                  id={courrier1.value}
+                  data-testid="courrier-element"
+                  className="email-link"
+                  to={`/outils/courrierGenerator/${courrier1.value}`}
+                >
+                  {courrier1.question}
+                </Link>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
