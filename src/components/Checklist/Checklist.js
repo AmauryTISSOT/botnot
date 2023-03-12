b0fada5b95a627231419e3cc2d65eae711e95ba1
amaury <tissot.amaury@gmail.com>
2023-03-12 17:52:13 +0100

    Add checklist reset button & remove react warning

--- src/components/Checklist/Checklist.js
+++ src/components/Checklist/Checklist.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import NoMatch from "../../pages/NoMatch/NoMatch";
-import "./Checklist.css"
+import "./Checklist.css";
 import checkListData from "./ChecklistData/ChecklistData";
 
 const Checklist = () => {
-  const {checkListId} = useParams();
-  const [checklistItems, setChecklistItems] = useState(checkListData[checkListId]);
+  const { checkListId } = useParams();
+  const [checklistItems, setChecklistItems] = useState(
+    checkListData[checkListId]
+  );
 
   const handleCheckboxClick = (id) => {
     setChecklistItems(
@@ -19,35 +21,51 @@ const Checklist = () => {
     );
   };
 
+  const resetChecklist = () => {
+    setChecklistItems(
+      checklistItems.map((item) => {
+        if (item.checked === true) {
+          item.checked = false;
+        }
+        return item;
+      })
+    );
+  };
+
   try {
     return (
-      <div className="checklist-wrap">
-        <ul className="checklist-container">
-          {checklistItems.map((item) => (
-            <li key={item.id} className="checklist-list">
-              <input
-                className="checkbox"
-                type="checkbox"
-                checked={item.checked}
-                onClick={() => handleCheckboxClick(item.id)
-                }
-              />
-              {console.log( item.id, item.checked)}
-              <label
-                data-testid={item.id}
-                style={{ textDecoration: item.checked ? "line-through" : "none" }}
-                onClick={() => handleCheckboxClick(item.id)}
-              >
-                {item.text}
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <>
+        <div className="checklist-wrap">
+          <ul className="checklist-container">
+            {checklistItems.map((item) => (
+              <li key={item.id} className="checklist-list">
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  checked={item.checked}
+                  onChange={() => handleCheckboxClick(item.id)}
+                />
+                <label
+                  data-testid={item.id}
+                  style={{
+                    textDecoration: item.checked ? "line-through" : "none",
+                  }}
+                  onClick={() => handleCheckboxClick(item.id)}
+                >
+                  {item.text}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button className="checklist-reset" onClick={resetChecklist}>
+          Reset
+        </button>
+      </>
     );
   } catch (error) {
-    console.warn(error.message)
-    return <NoMatch/>
+    console.warn(error.message);
+    return <NoMatch />;
   }
 };
 
