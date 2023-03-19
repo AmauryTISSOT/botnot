944524c80c635f1f2898b5bc74337d80ef985fee
amaury <tissot.amaury@gmail.com>
2023-03-19 17:01:00 +0100

    Add new test

--- src/components/CourrierGenerator/CourrierComponent/CourrierForm/CourrierForm.test.js
+++ src/components/CourrierGenerator/CourrierComponent/CourrierForm/CourrierForm.test.js
@@ -23,6 +23,17 @@ describe('CourrierForm unit testing', () => {
     render(<CourrierForm />);
     const etatCivilInput = screen.getByLabelText(/ETAT-CIVIL/i);
     fireEvent.change(etatCivilInput, {
+      target: { value: "Madame Danielle Marie Jeanne DUPONT, Directrice d'usine, demeurant à PARIS (75001) 1 rue inconnue.  Née à BOURG-ARGENTAL (42220) le 4 septembre 1900.  Divorcée de Monsieur Jean-Yves PATRICK suivant jugement rendu par le tribunal  de SAINT ETIENNE (00000) le 19 octobre 1980, et non remariée.  Non liée par un pacte civil de solidarité.  De nationalité française.  Résidente au sens de la réglementation fiscale" },
+    });
+    fireEvent.submit(etatCivilInput);
+    const etatCivilObject = screen.getByText(/prénom/i);
+    expect(etatCivilObject).toBeInTheDocument();
+  });
+
+  test('renders error text for etatCivil after form submission', () => {
+    render(<CourrierForm />);
+    const etatCivilInput = screen.getByLabelText(/ETAT-CIVIL/i);
+    fireEvent.change(etatCivilInput, {
       target: { value: 'Test etatCivil input' },
     });
     fireEvent.submit(etatCivilInput);
@@ -31,6 +42,17 @@ describe('CourrierForm unit testing', () => {
   });
 
   test('renders bienImmo array after form submission', () => {
+    render(<CourrierForm />);
+    const bienImmoInput = screen.getByLabelText(/BIEN IMMOBILIER/i);
+    fireEvent.change(bienImmoInput, {
+      target: { value: "DESIGNATION Dans un ensemble immobilier situé à PARIS 12ÈME ARRONDISSEMENT 75012 57 Boulevard de Poniatowski, et 7 A rue Claude Decaen.       Figurant ainsi au cadastre : Section	N°	Lieudit	Surface BN	33	7 A RUE CLAUDE DECAEN	00 ha 22 a 11 ca  Un extrait de plan cadastral est annexé.  Les lots de copropriété suivants :" },
+    });
+    fireEvent.submit(bienImmoInput);
+    const bienImmoArray = screen.getByText(/section/i);
+    expect(bienImmoArray).toBeInTheDocument();
+  });
+
+  test('renders error text for bienImmo form submission', () => {
     render(<CourrierForm />);
     const bienImmoInput = screen.getByLabelText(/BIEN IMMOBILIER/i);
     fireEvent.change(bienImmoInput, {
