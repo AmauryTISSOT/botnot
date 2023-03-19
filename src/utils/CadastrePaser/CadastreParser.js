292009732d6b4604762bf50009a2e58f6c0a3132
amaury <tissot.amaury@gmail.com>
2023-03-19 16:52:13 +0100

    Add address parser

--- src/utils/CadastrePaser/CadastreParser.js
+++ src/utils/CadastrePaser/CadastreParser.js
@@ -1,10 +1,30 @@
 const CadastreParser = (data) => {
-  const prefixeExist = data.includes("préfixe");
+  //const prefixeExist = data.includes("préfixe");
   //TODO: Add prefixe logic
 
   if (!data.includes("Section")) {
-    throw SyntaxError
+    throw SyntaxError;
+  }
+
+  const extractAddress = () => {
+    const regex = /à\s(.*?)Figurant/; // find the text beetween "à" and "Figurant"
+    const match = data.match(regex);
+    const rawAddress = match[1].replace(".", "").trim();
+    const postalCode = rawAddress.match(/\b\d{5}\b/)[0]; // find the postal code
+    const streetName = rawAddress.match(/\b\d{5}\b\s(.+)/)[1].trim(); // find the street name and remove the dot "." at the end
+    let townName = rawAddress.match(/^\s*(.+)\s+\d{5}\b/)[1].trim();
+    if (townName.includes("(")) {
+      townName = rawAddress.match(/^[^(]*/)[0].trim(); //remove the "département name"
+    }
+
+    return {
+      rawAddress,
+      townName,
+      postalCode,
+      streetName,
+    };
   };
+
 
   const cleanArray = () => {
     // Remove tab
@@ -49,7 +69,10 @@ const CadastreParser = (data) => {
     }));
   };
 
-  return Parser(cadastreArray());
+  return {
+    address: extractAddress(),
+    cadastre: Parser(cadastreArray()),
+  };
 };
 
 export default CadastreParser;
