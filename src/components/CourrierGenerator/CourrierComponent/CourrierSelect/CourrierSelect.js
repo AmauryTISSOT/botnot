acf434cc236a2e58d01c2139ed28d8a8ab887236
amaury <tissot.amaury@gmail.com>
2023-03-21 22:55:32 +0100

    Rework CourrierSelect component

--- src/components/CourrierGenerator/CourrierComponent/CourrierSelect/CourrierSelect.js
+++ src/components/CourrierGenerator/CourrierComponent/CourrierSelect/CourrierSelect.js
@@ -1,31 +1,59 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import courrierListData from "../CourrierData/CourrierListData";
+import GenerateDocs from "../../GenerateDocs/GenerateDocs";
 
-const CourrierSelect = () => {
+const CourrierSelect = ({etatCivil, bienImmo}) => {
+// TODO: create a download folder with the selected queries
+
+const [selectedQueries, setSelectedQueries] = useState([]);
+
+  const handleCheckboxChange = (event) => {
+    const queryValue = event.target.value;
+    setSelectedQueries((prevSelectedQueries) => {
+      if (prevSelectedQueries.includes(queryValue)) {  
+        return prevSelectedQueries.filter((value) => value !== queryValue);// If the selected query is already in the 'selectedQueries' array, remove it
+      } else {
+        return [...prevSelectedQueries, queryValue];
+      }
+    });
+  };
+
+  const handleGenerateClick = () => {
+    console.log('Selected queries:', selectedQueries);
+    // GenerateDocs(
+    //   "Adresse Postale",
+    //   selectedQueries[0],
+    //   bienImmo.cadastre
+    // );
+    //TODO: revoir l'object "clientObject" dans GenerateDocs
+    console.log("bienImmo", bienImmo.cadastre)
+    console.log("etatCivil", etatCivil)
+  };
+
   return (
-    <>
-      <h1>Sélectionner un type de courrier</h1>
-      <div className="question-container">
-        {Object.values(courrierListData).map((courrier) => (
-          <div className="question-list" key={courrier.type.toString()}>
-            <h2>{courrier.type}</h2>
-            <ul className="question-link-list">
-              {courrier.query.map((courrier1, number1) => (
-                <Link
-                  key={number1}
-                  id={courrier1.value}
-                  data-testid="courrier-element"
-                  className="email-link"
-                  to={`/outils/courrierGenerator/${courrier1.value}`}
-                >
-                  {courrier1.question}
-                </Link>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-    </>
+    <div>
+      {courrierListData.map((category) => (
+        <div key={category.type}>
+          <h3>{category.type}</h3>
+          <ul>
+            {category.query.map((query) => (
+              <li key={query.value}>
+                <label>
+                  {query.question}
+                  <input
+                    type="checkbox"
+                    value={query.value}
+                    checked={selectedQueries.includes(query.value)}
+                    onChange={handleCheckboxChange}
+                  />
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <button onClick={handleGenerateClick}>Générer</button>
+    </div>  
   );
 };
 
