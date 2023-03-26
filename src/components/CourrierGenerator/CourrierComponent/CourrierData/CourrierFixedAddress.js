479cb50c8a8ea76528b3b932164b17eb14bfcfa5
amaury <tissot.amaury@gmail.com>
2023-03-26 22:29:51 +0200

    Create FixedAddress data

---
+++ src/components/CourrierGenerator/CourrierComponent/CourrierData/CourrierFixedAddress.js
@@ -0,0 +1,11 @@
+const CourrierFixedAddress = {
+  DLH: [
+    "DIRECTION DU LOGEMENT ET DE L’HABITAT",
+    "Sous-direction de l’habitat",
+    "Service technique de l’habitat",
+    "103 avenue de France",
+    "75013 PARIS ",
+  ],
+};
+
+export default CourrierFixedAddress;
