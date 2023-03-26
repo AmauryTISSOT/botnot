e10d4be4d59d50368bac3dc7c66a7e0979a96abe
amaury <tissot.amaury@gmail.com>
2023-03-26 21:33:13 +0200

    Rework for handling "mairie" address

--- src/components/CourrierGenerator/CourrierComponent/CourrierAdresse.js
+++ src/components/CourrierGenerator/CourrierComponent/CourrierAdresse.js
@@ -1,16 +1,52 @@
 import { Paragraph, TextRun } from "docx";
 import { convertMillimetersToTwip } from "docx";
+import GetMairieAddress from "../../API/GetMairieAddress/GetMairieAddress";
 
-const CourrierAdresse = (adresse) => {
-  return new Paragraph({
-    style: 'classic',
-    indent: { left: convertMillimetersToTwip(62.4) },
-    children: [
-      new TextRun({
-        text: adresse
+const CourrierAdresse = async (postalType, postalData) => {
+  if (postalType === "mairie") {
+    const postalObject = await GetMairieAddress(
+      postalData.address.postalCode,
+      postalData.address.townName
+    );
+    return [
+      new Paragraph({
+        style: "classic",
+        indent: { left: convertMillimetersToTwip(62.4) },
+        children: [
+          new TextRun({
+            text: "Mairie",
+          }),
+        ],
       }),
-    ],
-  });
+      new Paragraph({
+        style: "classic",
+        indent: { left: convertMillimetersToTwip(62.4) },
+        children: [
+          new TextRun({
+            text: postalObject.rue,
+          }),
+        ],
+      }),
+      new Paragraph({
+        style: "classic",
+        indent: { left: convertMillimetersToTwip(62.4) },
+        children: [
+          new TextRun({
+            text: `${postalObject.codePostal} - ${postalObject.commune}`,
+          }),
+        ],
+      }),
+    ];
+  } else
+    return new Paragraph({
+      style: "classic",
+      indent: { left: convertMillimetersToTwip(62.4) },
+      children: [
+        new TextRun({
+          text: postalType,
+        }),
+      ],
+    });
 };
 
 export default CourrierAdresse;
