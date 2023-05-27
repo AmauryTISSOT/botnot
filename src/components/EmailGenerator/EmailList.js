5d1e5e2a9cf179af1edd54a12cbeab3ca6aabf7d
amaury <tissot.amaury@gmail.com>
2023-05-28 00:02:37 +0200

    Rework data

--- src/components/EmailGenerator/EmailList.js
+++ src/components/EmailGenerator/EmailList.js
@@ -1,140 +1,273 @@
 const emailListData = [
   {
-    type: "Banque",
-    query: [
+    title: "Banque",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "deblocagePret",
-        question: "Demande de déblocage de prêt",
+        path: "deblocagePret",
+        name: "Demande de déblocage de prêt",
       },
     ],
   },
   {
-    type: "Syndic",
-    query: [
+    title: "Syndic",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 013.75 9.375v-4.5zM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 01-1.125-1.125v-4.5zM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0113.5 9.375v-4.5z"
+        />
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6.75 6.75h.75v.75h-.75v-.75zM6.75 16.5h.75v.75h-.75v-.75zM16.5 6.75h.75v.75h-.75v-.75zM13.5 13.5h.75v.75h-.75v-.75zM13.5 19.5h.75v.75h-.75v-.75zM19.5 13.5h.75v.75h-.75v-.75zM19.5 19.5h.75v.75h-.75v-.75zM16.5 16.5h.75v.75h-.75v-.75z"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "demandePretEtatDate",
-        question: "Demande de prêt état daté",
+        path: "demandePretEtatDate",
+        name: "Demande de prêt état daté",
       },
       {
-        value: "relanceSyndic",
-        question: "Relance syndic",
-      },
-    ],
-  },
-  {
-    type: "Divers",
-    query: [
-      {
-        value: "digicode",
-        question: "Envoi information digicode",
-      },
-      {
-        value: "visio",
-        question: "Visioconférence",
-      },
-      {
-        value: "copieActeSuiteSignature",
-        question: "Envoi copie d'acte suite à signature",
-      },
-      {
-        value: "copieActe",
-        question: "Demande copie d'acte",
-      },
-      {
-        value: "copiePlan",
-        question: "Demande copie plans",
-      },
-      {
-        value: "procuration",
-        question: "Envoi procuration",
-      },
-      {
-        value: "secret",
-        question: "Réponse secret professionnel",
-      },
-      {
-        value: "matriceCadastrale",
-        question: "Demande de matrice cadastrale",
-      },
-      {
-        value: "projet",
-        question: "Envoi projet d'acte",
+        path: "relanceSyndic",
+        name: "Relance syndic",
       },
     ],
   },
   {
-    type: "Comptabilité",
-    query: [
+    title: "Divers",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "soldeCompte",
-        question: "Demande de RIB pour solde de compte",
+        path: "digicode",
+        name: "Envoi information digicode",
+      },
+      {
+        path: "visio",
+        name: "Visioconférence",
+      },
+      {
+        path: "copieActeSuiteSignature",
+        name: "Envoi copie d'acte suite à signature",
+      },
+      {
+        path: "copieActe",
+        name: "Demande copie d'acte",
+      },
+      {
+        path: "copiePlan",
+        name: "Demande copie plans",
+      },
+      {
+        path: "procuration",
+        name: "Envoi procuration",
+      },
+      {
+        path: "secret",
+        name: "Réponse secret professionnel",
+      },
+      {
+        path: "matriceCadastrale",
+        name: "Demande de matrice cadastrale",
+      },
+      {
+        path: "projet",
+        name: "Envoi projet d'acte",
       },
     ],
   },
   {
-    type: "Vente",
-    query: [
+    title: "Comptabilité",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "envoiDecompteAcquereur",
-        question: "Envoi décompte acquéreur - déblocage des fonds",
-      },
-      {
-        value: "relanceDeblocageFonds",
-        question: "Relance déblocage des fonds",
-      },
-      {
-        value: "suiteSignaturePuv",
-        question: "Suite signature PUV - information",
-      },
-      {
-        value: "notificationSRU",
-        question: "Notification SRU",
-      },
-      {
-        value: "attestationTravaux",
-        question: "Travaux : demande d'attestation d'assurance décennale",
-      },
-    ],
-  },
-  {
-    type: "Succession",
-    query: [
-      {
-        value: "demandeTestament",
-        question: "Demande testament / DEE",
+        path: "soldeCompte",
+        name: "Demande de RIB pour solde de compte",
       },
     ],
   },
   {
-    type: "Urbanisme / construction",
-    query: [
+    title: "Vente",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "DHL",
-        question: "DLH PARIS : demande d'information",
-      },      
-      {
-        value: "infoDPU",
-        question: "Demande d'information DPU",
+        path: "envoiDecompteAcquereur",
+        name: "Envoi décompte acquéreur - déblocage des fonds",
       },
       {
-        value: "assDecennale",
-        question: "Demande d'assurance décennale",
+        path: "relanceDeblocageFonds",
+        name: "Relance déblocage des fonds",
+      },
+      {
+        path: "suiteSignaturePuv",
+        name: "Suite signature PUV - information",
+      },
+      {
+        path: "notificationSRU",
+        name: "Notification SRU",
+      },
+      {
+        path: "attestationTravaux",
+        name: "Travaux : demande d'attestation d'assurance décennale",
       },
     ],
   },
   {
-  type: "Modifcatif EDD",
-  query: [
-    {
-      value: "modifEDDPieceSyndic",
-      question: "Demande des pièces au syndic"
-    },
-    {
-      value: "modifEDDDemandeGeometre",
-      question: "Demande du modificatif WORD au géomètre"
-    }
-  ]
-  }
+    title: "Succession",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+        />
+      </svg>
+    ),
+    content: [
+      {
+        path: "demandeTestament",
+        name: "Demande testament / DEE",
+      },
+    ],
+  },
+  {
+    title: "Urbanisme / construction",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+        />
+      </svg>
+    ),
+    content: [
+      {
+        path: "DHL",
+        name: "DLH PARIS : demande d'information",
+      },
+      {
+        path: "infoDPU",
+        name: "Demande d'information DPU",
+      },
+      {
+        path: "assDecennale",
+        name: "Demande d'assurance décennale",
+      },
+    ],
+  },
+  {
+    title: "Modifcatif EDD",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"
+        />
+      </svg>
+    ),
+    content: [
+      {
+        path: "modifEDDPieceSyndic",
+        name: "Demande des pièces au syndic",
+      },
+      {
+        path: "modifEDDDemandeGeometre",
+        name: "Demande du modificatif WORD au géomètre",
+      },
+    ],
+  },
 ];
 
 export default emailListData;
