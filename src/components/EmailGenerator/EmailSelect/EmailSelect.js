17699a21ecad3dc17b7cefcc167f000b421436a4
amaury <tissot.amaury@gmail.com>
2023-05-28 00:03:29 +0200

    Add Accordion

--- src/components/EmailGenerator/EmailSelect/EmailSelect.js
+++ src/components/EmailGenerator/EmailSelect/EmailSelect.js
@@ -1,32 +1,18 @@
 import "./EmailSelect.css";
 import emailListData from "../EmailList";
-import { Link } from "react-router-dom";
+import Accordion from "../../../utils/Accordion/Accordion";
 
 const EmailSelect = () => {
   return (
-    <>
-      <h1>Sélectionner un modèle de mail</h1>
-      <div className="question-container">
-        {Object.values(emailListData).map((email, number) => (
-          <div className="question-list" key={email.type.toString()}>
-            <h2>{email.type}</h2>
-            <ul className="question-link-list">
-              {email.query.map((email1, number1) => (
-                <Link
-                  key={number1}
-                  id={email1.value}
-                  data-testid="question-element"
-                  className="email-link"
-                  to={`/outils/emailGenerator/${email1.value}`}
-                >
-                  {email1.question}
-                </Link>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center w-screen">
+      <h1 className="m-6 text-xl font-semibold">
+        Sélectionner un modèle de mail
+      </h1>
+      <Accordion
+        dataObject={emailListData}
+        rootPath={"/outils/emailGenerator/"}
+      />
+    </div>
   );
 };
 
