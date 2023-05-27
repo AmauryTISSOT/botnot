2f13b25e106826f5e9721780f42f3d9d181b114b
amaury <tissot.amaury@gmail.com>
2023-05-28 00:02:22 +0200

    Add Accordion

--- src/components/Checklist/ChecklistSelect/ChecklistSelect.js
+++ src/components/Checklist/ChecklistSelect/ChecklistSelect.js
@@ -4,10 +4,13 @@ import Accordion from "../../../utils/Accordion/Accordion";
 const ChecklistSelect = () => {
   return (
     <div className="flex flex-col items-center justify-center w-screen">
-      <h1 className="m-6 text-xl font-semibold">Sélectionner un type de checklist</h1>
-      <div>
-            <Accordion dataObject={checklistList} rootPath={"/outils/checklistGenerator/"}/>
-      </div>
+      <h1 className="m-6 text-xl font-semibold">
+        Sélectionner un type de checklist
+      </h1>
+      <Accordion
+        dataObject={checklistList}
+        rootPath={"/outils/checklistGenerator/"}
+      />
     </div>
   );
 };
