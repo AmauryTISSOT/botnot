cf49e618f9881f68e51186e82e8dd219a8ea1e89
amaury <tissot.amaury@gmail.com>
2023-05-27 22:46:39 +0200

    Add accordion

--- src/components/Checklist/ChecklistSelect/ChecklistSelect.js
+++ src/components/Checklist/ChecklistSelect/ChecklistSelect.js
@@ -1,31 +1,14 @@
-import { Link } from "react-router-dom";
 import checklistList from "../ChecklistData/ChecklistListData";
+import Accordion from "../../../utils/Accordion/Accordion";
 
 const ChecklistSelect = () => {
   return (
-    <>
-      <h1>Sélectionner un type de checklist</h1>
-      <div className="question-container">
-        {Object.values(checklistList).map((checklist) => (
-          <div className="question-list" key={checklist.type.toString()}>
-            <h2>{checklist.type}</h2>
-            <ul className="question-link-list">
-              {checklist.query.map((checklist1, number1) => (
-                <Link
-                  key={number1}
-                  id={checklist1.value}
-                  data-testid="question-element"
-                  className="email-link"
-                  to={`/outils/checklistGenerator/${checklist1.value}`}
-                >
-                  {checklist1.question}
-                </Link>
-              ))}
-            </ul>
-          </div>
-        ))}
+    <div className="flex flex-col items-center justify-center w-screen">
+      <h1 className="m-6 text-xl font-semibold">Sélectionner un type de checklist</h1>
+      <div>
+            <Accordion dataObject={checklistList} rootPath={"/outils/checklistGenerator/"}/>
       </div>
-    </>
+    </div>
   );
 };
 
