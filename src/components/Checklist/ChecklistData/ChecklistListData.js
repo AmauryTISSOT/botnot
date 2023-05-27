8caab0a8e9c6d4238dfd3e7f955e7e027f6de634
amaury <tissot.amaury@gmail.com>
2023-05-27 22:46:22 +0200

    Rewrite data

--- src/components/Checklist/ChecklistData/ChecklistListData.js
+++ src/components/Checklist/ChecklistData/ChecklistListData.js
@@ -1,103 +1,199 @@
 const checklistList = [
   {
-    type: "Urbanisme",
-    query: [
+    title: "Urbanisme",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "dia",
-        question: "Déclaration d'intention d'aliéner (DIA)",
+        path: "dia",
+        name: "Déclaration d'intention d'aliéner (DIA)",
       },
       {
-        value: "cu",
-        question: "Certificat d'urbanisme (CU)",
-      },
-    ],
-  },
-  {
-    type: "Divers",
-    query: [
-      {
-        value: "docusign",
-        question: "Procuration DocuSign",
-      },
-      {
-        value: "ehf",
-        question: "Etat hypothécaire",
-      },
-      {
-        value: "decompte",
-        question: "Décompte",
-      },
-      {
-        value: "minute",
-        question: "Pochette Minute",
+        path: "cu",
+        name: "Certificat d'urbanisme (CU)",
       },
     ],
   },
   {
-    type: "Vente",
-    query: [
+    title: "Divers",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "analyseCompromisAgence",
-        question: "Analyse d'un compromis d'agence",
+        path: "docusign",
+        name: "Procuration DocuSign",
       },
       {
-        value: "fixationRdvVente",
-        question: "Fixation rendez-vous acte de vente",
+        path: "ehf",
+        name: "Etat hypothécaire",
+      },
+      {
+        path: "decompte",
+        name: "Décompte",
+      },
+      {
+        path: "minute",
+        name: "Pochette Minute",
+      },
+    ],
+  },
+  {
+    title: "Vente",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+        />
+      </svg>
+    ),
+    content: [
+      {
+        path: "analyseCompromisAgence",
+        name: "Analyse d'un compromis d'agence",
+      },
+      {
+        path: "fixationRdvVente",
+        name: "Fixation rendez-vous acte de vente",
       },
 
       {
-        value: "clauseEtude",
-        question: "Clauses étude à pointer",
+        path: "clauseEtude",
+        name: "Clauses étude à pointer",
       },
     ],
   },
   {
-    type: "Formalités postérieures",
-    query: [
+    title: "Formalités postérieures",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "formapuv",
-        question: "Formalités postérieure promesse de vente (PUV)",
+        path: "formapuv",
+        name: "Formalités postérieure promesse de vente (PUV)",
       },
       {
-        value: "formaVente",
-        question: "Formalités postérieure acte de vente",
+        path: "formaVente",
+        name: "Formalités postérieure acte de vente",
       },
       {
-        value: "formaPret",
-        question: "Formalités postérieure acte de prêt",
+        path: "formaPret",
+        name: "Formalités postérieure acte de prêt",
       },
     ],
   },
   {
-    type: "Formalités antérieures",
-    query: [
+    title: "Formalités antérieures",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "formaAntPuv",
-        question: "Formalités antérieures promesse de vente (PUV)"
+        path: "formaAntPuv",
+        name: "Formalités antérieures promesse de vente (PUV)",
       },
       {
-        value: "formaAntVente",
-        question: "Formalités antérieures promesse acte de vente"
+        path: "formaAntVente",
+        name: "Formalités antérieures promesse acte de vente",
       },
       {
-        value: "formaAntPret",
-        question: "Formalités antérieures acte de prêt"
+        path: "formaAntPret",
+        name: "Formalités antérieures acte de prêt",
       },
-    ]
+    ],
   },
   {
-    type: "Comptabilité",
-    query: [
+    title: "Comptabilité",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
+    content: [
       {
-        value: "soldeCompte",
-        question: "Solde de compte"
+        path: "soldeCompte",
+        name: "Solde de compte",
       },
       {
-        value: "taxeVente",
-        question: "Taxe d'une vente"
-      }
-    ]
-  }
+        path: "taxeVente",
+        name: "Taxe d'une vente",
+      },
+    ],
+  },
 ];
 
 export default checklistList;
